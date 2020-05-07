8de29c060b77e02d97f7521b3b66b6130e27eeaf
Modus-Exciter <glorguin@yandex.ru>
2020-05-07 23:22:46 +0400

    Создано решение и проекты. Концептуальные названия проектов.

---
+++ source/NDTM/Fixture.Conch/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Fixture.Conch
+{
+  public class Class1
+  {
+  }
+}
---
+++ source/NDTM/Norn/Scenario.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Norn
+{
+  public class Scenario
+  {
+  }
+}
