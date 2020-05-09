57934ac70cf772747f8a6d2e1db8de89f013832a
Modus-Exciter <glorguin@yandex.ru>
2020-05-09 21:39:27 +0400

    Переименовка

---
+++ source/Notung/Logging/ILog.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Notung.Log
+{
+  public interface ILog
+  {
+    void WriteLog(string message, InfoLevel level, object data = null);
+  } 
+
+  /// <summary>
+  /// Смысл этой структуры - запомнить всё, что требуется писать в лог 
+  /// (к стандартному комплекту Info добавляется источник и дата события)
+  /// </summary>
+  public struct LoggingData
+  {
+    public LoggingData(string source, string message, InfoLevel level, object data) : this()
+    {
+      if (string.IsNullOrEmpty(source))
+        throw new ArgumentNullException("source"); 
+
+      if (string.IsNullOrEmpty(message))
+        throw new ArgumentNullException("message");
+
+      this.Source = source;
+      this.Message = message;
+      this.Level = level;
+      this.Data = data;
+      this.LoggingDate = DateTime.Now;
+    }
+    
+    public readonly string Message;
+
+    public readonly InfoLevel Level;
+
+    public readonly object Data;
+
+    public readonly DateTime LoggingDate;
+
+    public readonly string Source;
+
+    public override string ToString()
+    {
+      if (!string.IsNullOrWhiteSpace(this.Source))
+        return string.Format("{0}: {1}: {2}", this.Source, this.Level, this.Message);
+      else
+        return string.Format("{0}: {1}", this.Level, this.Message);
+    }
+  }
+}
---
+++ source/Notung/Logging/LogManager.cs
@@ -0,0 +1,120 @@
+﻿using System;
+
+namespace Notung.Log
+{
+  /// <summary>
+  /// Пока не решил, использовать log4net или свои асинхронные логи, частично дублируем его API
+  /// </summary> 
+  public static class LogManager
+  {
+    public static ILog GetLogger(string source)
+    {
+      if (string.IsNullOrEmpty(source))
+        throw new ArgumentNullException("name");
+
+      return new Logger(source);
+    }
+
+    public static ILog GetLogger(Type type)
+    {
+      return new Logger(type.FullName);
+    }
+
+    #region Extensions
+
+    public static void Alert(this ILog logger, Info info)
+    {
+      logger.WriteLog(info.Message, info.Level, info.Details);
+
+      foreach (var inner in info.InnerMessages)
+        logger.Alert(inner);
+    }
+
+    public static void Debug(this ILog logger, string message)
+    {
+      logger.WriteLog(message, InfoLevel.Debug);
+    }
+
+    public static void Info(this ILog logger, string message)
+    {
+      logger.WriteLog(message, InfoLevel.Info);
+    }
+
+    public static void Warn(this ILog logger, string message)
+    {
+      logger.WriteLog(message, InfoLevel.Warning);
+    }
+
+    public static void Warn(this ILog logger, string message, Exception ex)
+    {
+      logger.WriteLog(message, InfoLevel.Warning, ex);
+    }
+
+    public static void Error(this ILog logger, string message)
+    {
+      logger.WriteLog(message, InfoLevel.Error);
+    }
+
+    public static void Error(this ILog logger, string message, Exception ex)
+    {
+      logger.WriteLog(message, InfoLevel.Error, ex);
+    }
+
+    public static void Fatal(this ILog logger, string message)
+    {
+      logger.WriteLog(message, InfoLevel.Fatal);
+    }
+
+    public static void Fatal(this ILog logger, string message, Exception ex)
+    {
+      logger.WriteLog(message, InfoLevel.Fatal, ex);
+    }
+
+    public static void DebugFormat(this ILog logger, string format, params object[] args)
+    {
+      logger.WriteLog(string.Format(format, args), InfoLevel.Debug);
+    }
+
+    public static void InfoFormat(this ILog logger, string format, params object[] args)
+    {
+      logger.WriteLog(string.Format(format, args), InfoLevel.Info);
+    }
+
+    public static void WarnFormat(this ILog logger, string format, params object[] args)
+    {
+      logger.WriteLog(string.Format(format, args), InfoLevel.Warning);
+    }
+
+    public static void ErrorFormat(this ILog logger, string format, params object[] args)
+    {
+      logger.WriteLog(string.Format(format, args), InfoLevel.Error);
+    }
+
+    public static void FatalFormat(this ILog logger, string format, params object[] args)
+    {
+      logger.WriteLog(string.Format(format, args), InfoLevel.Fatal);
+    }
+
+    #endregion
+
+    private class Logger : ILog
+    {
+      private readonly string m_source;
+
+      public Logger(string source)
+      {
+        m_source = source;
+      }
+
+      public override string ToString()
+      {
+        return string.Format("ILog({0})", m_source);
+      }
+
+      public void WriteLog(string message, InfoLevel level, object data)
+      {
+        // TODO: for async logs knowing of main thread is required
+      }
+    }
+  }
+}
