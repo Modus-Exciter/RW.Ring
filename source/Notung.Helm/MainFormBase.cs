649f06db5589dac6c7c0cf1d6ad1cb7f0fcaf5c9
Modus-Exciter <glorguin@yandex.ru>
2020-05-11 02:05:13 +0400

    Довольно бессмысленная форма

---
+++ source/Notung.Helm/MainFormBase.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Notung.Helm
+{
+  public partial class MainFormBase : Form
+  {
+    public MainFormBase()
+    {
+      InitializeComponent();
+    }
+
+    protected override void WndProc(ref Message msg)
+    {
+      base.WndProc(ref msg);
+
+      if (msg.Msg == MainFormAppInstanceView.StringArgsMessageCode)
+      {
+        AcceptStringArgs(MainFormAppInstanceView.GetStringArgs(msg));
+      }
+    }
+
+    protected virtual void AcceptStringArgs(string[] args) { }
+  }
+}
