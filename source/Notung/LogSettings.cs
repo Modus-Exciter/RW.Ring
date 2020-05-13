48cd5f0c797aa845643b49f022d41cab4e1c522a
Modus-Exciter <glorguin@yandex.ru>
2020-05-13 11:08:47 +0400

    Format

--- source/Notung/LogSettings.cs
+++ source/Notung/LogSettings.cs
@@ -4,40 +4,31 @@ using System.IO;
 using Notung.Log;
 using Notung.Properties;
 
-namespace Notung {
-    
-    
-    // This class allows you to handle specific events on the settings class:
-    //  The SettingChanging event is raised before a setting's value is changed.
-    //  The PropertyChanged event is raised after a setting's value is changed.
-    //  The SettingsLoaded event is raised after the setting values are loaded.
-    //  The SettingsSaving event is raised before the setting values are saved.
-    internal sealed partial class LogSettings {
+namespace Notung
+{
+  // This class allows you to handle specific events on the settings class:
+  //  The SettingChanging event is raised before a setting's value is changed.
+  //  The PropertyChanged event is raised after a setting's value is changed.
+  //  The SettingsLoaded event is raised after the setting values are loaded.
+  //  The SettingsSaving event is raised before the setting values are saved.
+  internal sealed partial class LogSettings
+  {
+    private LogStringBuilder m_builder;
 
-      private readonly object m_lock = new object();
+    public void BuildString(TextWriter writer, LoggingData data)
+    {
+      if (m_builder == null)
+        m_builder = new LogStringBuilder(MessageTemplate);
 
-      private LogStringBuilder m_builder;
-
-      public void BuildString(TextWriter writer, LoggingData data)
-      {
-        if (m_builder == null)
-        {
-          lock (m_lock)
-          {
-            if (m_builder == null)
-              m_builder = new LogStringBuilder(MessageTemplate);
-          }
-        }
-
-        m_builder.BuildString(writer, data);
-      }
-
-      protected override void OnSettingsLoaded(object sender, System.Configuration.SettingsLoadedEventArgs e)
-      {
-        base.OnSettingsLoaded(sender, e);
-
-        if (string.IsNullOrWhiteSpace(this.DefaultLogger))
-          throw new ConfigurationErrorsException(Resources.EMPTY_DEFAULT_LOGGER);
-      }
+      m_builder.BuildString(writer, data);
     }
+
+    protected override void OnSettingsLoaded(object sender, System.Configuration.SettingsLoadedEventArgs e)
+    {
+      base.OnSettingsLoaded(sender, e);
+
+      if (string.IsNullOrWhiteSpace(this.DefaultLogger))
+        throw new ConfigurationErrorsException(Resources.EMPTY_DEFAULT_LOGGER);
+    }
+  }
 }
