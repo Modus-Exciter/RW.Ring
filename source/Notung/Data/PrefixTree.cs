4ebdb379b0d6368301bb1b4ed2e8e65c66325889
Modus-Exciter <glorguin@yandex.ru>
2020-05-15 20:16:46 +0400

    PrefixTree: added new features

--- source/Notung/Data/PrefixTree.cs
+++ source/Notung/Data/PrefixTree.cs
@@ -1,27 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 
 namespace Notung.Data
 {
+  /// <summary>
+  /// Класс для быстрого поиска префиксов строки
+  /// </summary>
   public class PrefixTree
   {
     private readonly PrefixTreeItem m_root = new PrefixTreeItem('\0');
+    private int m_count;
 
+    /// <summary>
+    /// Добавление префикса для поиска
+    /// </summary>
+    /// <param name="prefix"></param>
     public void AddPrefix(string prefix)
     {
       if (string.IsNullOrEmpty(prefix))
         throw new ArgumentNullException("prefix");
 
       var item = m_root;
-
+      var addedNew = false;
       for (int i = 0; i < prefix.Length; i++)
-        item = item.AddChild(prefix[i]);
+        item = item.AddChild(prefix[i], out addedNew);
+
+      if (addedNew)
+        m_count++;
     }
 
+    /// <summary>
+    /// Проверяет, что переданная строка начинается с одного из заведённых префиксов
+    /// </summary>
+    /// <param name="fullString">Строка для проверки</param>
+    /// <returns>Если строка начинается хотя бы с одного из префиксов, то true. Иначе, false</returns>
     public bool MatchAny(string fullString)
     {
+      if (string.IsNullOrEmpty(fullString))
+        throw new ArgumentNullException("fullString");
+      
       var item = m_root;
 
       for (int i = 0; i < fullString.Length; i++)
@@ -35,9 +53,51 @@ namespace Notung.Data
       return false;
     }
 
+    /// <summary>
+    /// Очищает список префиксов
+    /// </summary>
     public void Clear()
     {
       m_root.Clear();
+      m_count = 0;
+    }
+
+    /// <summary>
+    /// Получение массива всех префиксов, которые есть
+    /// </summary>
+    /// <returns>Все префиксы, ранее добавленные методом Add</returns>
+    public string[] GetAllPrefixes()
+    {
+      LinkedList<PrefixTreeItem> list = new LinkedList<PrefixTreeItem>();
+      Collect(m_root, list);
+
+      var ret = new string[list.Count];
+
+      int i = 0;
+
+      foreach (var item in list)
+        ret[i++] = item.ToString();
+
+      return ret;
+    }
+
+    /// <summary>
+    /// Общее количество префиксов
+    /// </summary>
+    public int Count
+    {
+      get { return m_count; }
+    }
+
+    private void Collect(PrefixTreeItem item, LinkedList<PrefixTreeItem> list)
+    {
+      foreach (var child in item.Children)
+      {
+        if (child.IsLeaf)
+          list.AddLast(child);
+        else
+          Collect(child, list);
+      }
     }
 
     private class PrefixTreeItem
@@ -51,15 +111,18 @@ namespace Notung.Data
         m_symbol = symbol;
       }
 
-      public PrefixTreeItem AddChild(char symbol)
+      public PrefixTreeItem AddChild(char symbol, out bool addedNew)
       {
         PrefixTreeItem child;
+
+        addedNew = false;
 
         if (!m_next_symbols.TryGetValue(symbol, out child))
         {
           child = new PrefixTreeItem(symbol);
           child.m_parent = this;
           m_next_symbols.Add(symbol, child);
+          addedNew = true;
         }
 
         return child;
@@ -78,6 +141,11 @@ namespace Notung.Data
       public bool TryGetChild(char symbol, out PrefixTreeItem item)
       {
         return m_next_symbols.TryGetValue(symbol, out item);
+      }
+
+      public ICollection<PrefixTreeItem> Children
+      {
+        get { return m_next_symbols.Values; }
       }
 
       public PrefixTreeItem Parent
@@ -105,17 +173,23 @@ namespace Notung.Data
 
       public override string ToString()
       {
-        StringBuilder sb = new StringBuilder();
-
+        var list = new LinkedList<char>();
         var item = this;
 
         while (item.m_parent != null)
         {
-          sb.Append(item.m_symbol);
+          list.AddFirst(item.m_symbol);
           item = item.m_parent;
         }
 
-        return new string(sb.ToString().Reverse().ToArray());
+        var ret = new char[list.Count];
+
+        int i = 0;
+
+        foreach (var symbol in list)
+          ret[i++] = symbol;
+
+        return new string(ret);
       }
     }
   }
--- test/Notung/NotungTest/CommonTest.cs
+++ test/Notung/NotungTest/CommonTest.cs
@@ -3,6 +3,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Notung;
 using Notung.Data;
 using Notung.Loader;
+using System.Collections.Generic;
 
 namespace NotungTest
 {
@@ -29,6 +30,31 @@ namespace NotungTest
       Assert.IsFalse(tree.MatchAny("Dev.Express"));
       Assert.IsTrue(tree.MatchAny("System"));
       Assert.IsTrue(tree.MatchAny("DocumentView"));
+      Assert.AreEqual(4, tree.Count);
+
+      var hs = new HashSet<string>(tree.GetAllPrefixes());
+      Assert.AreEqual(4, hs.Count);
+
+      Assert.IsTrue(hs.Contains("DevExpress."));
+      Assert.IsTrue(hs.Contains("System"));
+      Assert.IsTrue(hs.Contains("Syslogs"));
+      Assert.IsTrue(hs.Contains("Document"));
+
+      tree.Clear();
+      Assert.AreEqual(0, tree.Count);
+      Assert.AreEqual(0, tree.GetAllPrefixes().Length);
+      Assert.IsFalse(tree.MatchAny("Anything"));
+    }
+
+    [TestMethod]
+    public void DuplicatePrefix()
+    {
+      PrefixTree tree = new PrefixTree();
+      tree.AddPrefix("DevExpress.");
+      tree.AddPrefix("System");
+      tree.AddPrefix("System");
+
+      Assert.AreEqual(2, tree.Count);
     }
 
     [TestMethod]
