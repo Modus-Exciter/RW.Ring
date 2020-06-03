a3358af1415c35679ae08a2bba6d4da84a133508
Modus-Exciter <glorguin@yandex.ru>
2020-06-03 22:56:32 +0400

    Base client-server interaction by TCP/IP with binary stream serialization

---
+++ source/Notung/Network/Host.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using Notung.Loader;
+
+namespace Notung.Network
+{
+  public class Host<TContract, TService> : IDisposable
+    where TContract : class
+    where TService : class, TContract, new()
+  {
+    private readonly HostedService<TContract, TService>[] m_services;
+    private readonly Thread[] m_threads;
+
+    public Host(params HostedService<TContract, TService>[] services)
+    {
+      if (services == null)
+        throw new ArgumentNullException("services");
+
+      m_services = services.Where(s => s != null).ToArray();
+      m_threads = new Thread[m_services.Length];
+
+      for (int i = 0; i < m_services.Length; i++)
+        m_threads[i] = new Thread(m_services[i].Listen);
+    }
+
+    public Host(IServerTransportFactory transportFactory, ICommandSerializer serializer)
+      : this(new HostedService<TContract, TService>(transportFactory, serializer)) { }
+
+    public void Start()
+    {
+      for (int i = 0; i < m_services.Length; i++)
+        m_threads[i].Start();
+    }
+
+    public void Dispose()
+    {
+      for (int i = 0; i < m_services.Length; i++)
+      {
+        m_services[i].Dispose();
+        m_threads[i].Abort();
+      }
+    }
+  }
+
+  public class HostedService<TContract, TService> : IDisposable
+    where TContract : class
+    where TService : class, TContract, new()
+  {
+    private readonly IServerTransportFactory m_transport_factory;
+    private readonly ICommandSerializer m_serializer;
+
+    public HostedService(IServerTransportFactory transportFactory, ICommandSerializer serializer)
+    {
+      if (transportFactory == null)
+        throw new ArgumentNullException("factory");
+
+      if (serializer == null)
+        throw new ArgumentNullException("serizlizer");
+
+      m_transport_factory = transportFactory;
+      m_serializer = serializer;
+    }
+
+    public void Listen()
+    {
+      while (true)
+        ThreadPool.QueueUserWorkItem(Accept, m_transport_factory.Create());
+    }
+
+    private void Accept(object state)
+    {
+      using (var transport = (ITransport)state)
+      {
+        var command = m_serializer.Deserialize<IRemotableCommand>(transport.RequestStream);
+
+        m_serializer.Serialize<RemotableResult>(transport.ResponseStream, command.Execute(
+          new CommandContext { HostType = typeof(TService) }));
+
+        transport.EndResponse();
+      }
+    }
+
+    public void Dispose()
+    {
+      m_transport_factory.Dispose();
+    }
+  }
+}
---
+++ source/Notung/Network/IRemotableCommand.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Runtime.Remoting.Messaging;
+using Notung.Loader;
+
+namespace Notung.Network
+{
+  /// <summary>
+  /// Команда, которую можно выполнить удалённо
+  /// </summary>
+  public interface IRemotableCommand
+  {
+    /// <summary>
+    /// Выполнение команды
+    /// </summary>
+    /// <param name="context">Контекст выполнения команды</param>
+    /// <returns>Результат выполнения команды</returns>
+    RemotableResult Execute(CommandContext context);
+  }
+
+  /// <summary>
+  /// Контекст выполнения команды
+  /// </summary>
+  public class CommandContext
+  {
+    /// <summary>
+    /// Тип хоста, выполняющего команду
+    /// </summary>
+    public Type HostType { get; set; }
+  }
+
+  [Serializable]
+  public class RemotableResult
+  {
+    public object ReturnValue { get; internal set; }
+
+    public object[] RefParameters { get; internal set; }
+
+    public Exception Exception { get; internal set; }
+
+    public MethodCallResultType Type { get; internal set; }
+  }
+
+  [Serializable]
+  public enum MethodCallResultType
+  {
+    Success,
+    Error,
+    Callback
+  }
+
+  /// <summary>
+  /// Обмен сообщениями с сервером
+  /// </summary>
+  /// <typeparam name="T">Тип контракта сервиса, с которому относятся сообщения</typeparam>
+  public interface IRemotableCaller
+  {
+    /// <summary>
+    /// Выполнение команды, потенциально удалённой
+    /// </summary>
+    /// <param name="command">Выполняемая команда</param>
+    /// <returns>Результат выполнения командыы</returns>
+    RemotableResult Call(IRemotableCommand command);
+  }
+
+  public sealed class RemotableCallerStub<TService> : IRemotableCaller
+  {
+    public RemotableResult Call(IRemotableCommand command)
+    {
+      return command.Execute(new CommandContext { HostType = typeof(TService) });
+    }
+  }
+
+  public class RemoteCaller : IRemotableCaller
+  {
+    private readonly IFactory<ITransport> m_transport_factory;
+    private readonly ICommandSerializer m_serializer;
+
+    public RemoteCaller(IFactory<ITransport> transportFactory, ICommandSerializer serializer)
+    {
+      if (transportFactory == null)
+        throw new ArgumentNullException("factory");
+
+      if (serializer == null)
+        throw new ArgumentNullException("serizlizer");
+
+      m_transport_factory = transportFactory;
+      m_serializer = serializer;
+    }
+    
+    public RemotableResult Call(IRemotableCommand command)
+    {
+      using (var transport = m_transport_factory.Create())
+      {
+        m_serializer.Serialize<IRemotableCommand>(transport.RequestStream, command);
+        transport.EndRequest();
+        return m_serializer.Deserialize<RemotableResult>(transport.ResponseStream);
+      }
+    }
+  }
+}
