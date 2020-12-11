195aefec38c7f4362d685e5437e2643f899c5750
Modus-Exciter <glorguin@yandex.ru>
2020-12-12 02:05:30 +0300

    Remotable command serialization via custom tuples

---
+++ source/Notung/Net/IParametersList.cs
@@ -0,0 +1,450 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using Notung.Data;
+using System.Diagnostics;
+using System.Runtime.Serialization;
+
+namespace Notung.Net
+{
+  /// <summary>
+  /// Объект для упаковки параметров удалённой операции
+  /// </summary>
+  public interface IParametersList
+  {
+    /// <summary>
+    /// Получает типы параметров удалённой операции
+    /// </summary>
+    /// <returns>Типы параметров удалённой операции</returns>
+    Type[] GetTypes();
+
+    /// <summary>
+    /// Получает значения параметров удалённой операции
+    /// </summary>
+    /// <returns>Значения параметров удалённой операции</returns>
+    object[] GetValues();
+  }
+
+  /// <summary>
+  /// Работа с параметрами удалённой операции
+  /// </summary>
+  [Serializable, DataContract(Name = "PL", Namespace = "")]
+  public sealed class ParametersList : IParametersList
+  {
+    private static readonly ParametersList _instance = new ParametersList();
+
+    private ParametersList() { }
+
+    /// <summary>
+    /// Преобразование метода и значений его параметров в объект упаковки
+    /// </summary>
+    /// <param name="method">Метод, для которого нужно сформировать набор параметров</param>
+    /// <param name="values">Значения параметров для метода</param>
+    /// <returns>Объект для упаковки параметров удалённой операции</returns>
+    public static IParametersList Create(MethodInfo method, params object[] values)
+    {
+      Debug.Assert(method != null);
+      Debug.Assert(values != null);
+
+      return (IParametersList)Activator.CreateInstance(GetRequiredType(method, values), values);
+    }
+
+    /// <summary>
+    /// Получение типа объекта для упаковки, подходящего для вызова удалённого метода
+    /// </summary>
+    /// <param name="method">метод, который нужно вызвать удалённо</param>
+    /// <returns>Тип объекта упаковки</returns>
+    public static Type GetRequiredType(MethodInfo method)
+    {
+      Debug.Assert(method != null);
+      return GetRequiredType(method, null);
+    }
+
+    private static Type GetRequiredType(MethodInfo method, object[] values)
+    {
+      var parameters = method.GetParameters();
+
+      if (values != null && values.Length != parameters.Length)
+        throw new ArgumentException("Parameters count mismatch");
+
+      if (parameters.Length == 0)
+        return typeof(ParametersList);
+
+      var types = new Type[parameters.Length];
+
+      for (int i = 0; i < parameters.Length; i++)
+      {
+        types[i] = parameters[i].ParameterType.IsByRef ?
+          parameters[i].ParameterType.GetElementType() : parameters[i].ParameterType;
+      }
+
+      Type baseType;
+      switch (types.Length)
+      {
+        case 1:
+          baseType = typeof(ParamList<>);
+          break;
+        case 2:
+          baseType = typeof(ParamList<,>);
+          break;
+        case 3:
+          baseType = typeof(ParamList<,,>);
+          break;
+        case 4:
+          baseType = typeof(ParamList<,,,>);
+          break;
+        case 5:
+          baseType = typeof(ParamList<,,,,>);
+          break;
+        case 6:
+          baseType = typeof(ParamList<,,,,,>);
+          break;
+        case 7:
+          baseType = typeof(ParamList<,,,,,,>);
+          break;
+        case 8:
+          baseType = typeof(ParamList<,,,,,,,>);
+          break;
+        default:
+          throw new ArgumentException("Too many parameters");
+      }
+
+      return baseType.MakeGenericType(types);
+    }
+
+    Type[] IParametersList.GetTypes()
+    {
+      return ArrayExtensions.Empty<Type>();
+    }
+
+    object[] IParametersList.GetValues()
+    {
+      return ArrayExtensions.Empty<object>();
+    }
+
+    [Serializable, DataContract]
+    private abstract class ParametersListBase : IParametersList
+    {
+      public abstract Type[] GetTypes();
+
+      public abstract object[] GetValues();
+
+      public sealed override string ToString()
+      {
+        return string.Join(", ", GetValues());
+      }
+    }
+
+    [Serializable, DataContract(Name = "PL", Namespace = "")]
+    private sealed class ParamList<T> : ParametersListBase
+    {
+      private static readonly Type[] _types = new Type[] { typeof(T) };
+
+      [DataMember(Name = "P", EmitDefaultValue = false)]
+      private readonly T m_1;
+
+      public ParamList(T p1)
+      {
+        m_1 = p1;
+      }
+
+      public override Type[] GetTypes()
+      {
+        return _types;
+      }
+
+      public override object[] GetValues()
+      {
+        return new object[] { m_1 };
+      }
+    }
+
+    [Serializable, DataContract(Name = "PL", Namespace = "")]
+    private sealed class ParamList<T1, T2> : ParametersListBase
+    {
+      private static readonly Type[] _types = new Type[] { typeof(T1), typeof(T2) };
+
+      [DataMember(Name = "A", EmitDefaultValue = false)]
+      private readonly T1 m_1;
+      [DataMember(Name = "B", EmitDefaultValue = false)]
+      private readonly T2 m_2;
+
+      public ParamList(T1 p1, T2 p2)
+      {
+        m_1 = p1;
+        m_2 = p2;
+      }
+
+      public override Type[] GetTypes()
+      {
+        return _types;
+      }
+
+      public override object[] GetValues()
+      {
+        return new object[] { m_1, m_2 };
+      }
+    }
+
+    [Serializable, DataContract(Name = "PL", Namespace = "")]
+    private sealed class ParamList<T1, T2, T3> : ParametersListBase
+    {
+      private static readonly Type[] _types = new Type[] { typeof(T1), typeof(T2), typeof(T3) };
+
+      [DataMember(Name = "A", EmitDefaultValue = false)]
+      private readonly T1 m_1;
+      [DataMember(Name = "B", EmitDefaultValue = false)]
+      private readonly T2 m_2;
+      [DataMember(Name = "C", EmitDefaultValue = false)]
+      private readonly T3 m_3;
+
+      public ParamList(T1 p1, T2 p2, T3 p3)
+      {
+        m_1 = p1;
+        m_2 = p2;
+        m_3 = p3;
+      }
+
+      public override Type[] GetTypes()
+      {
+        return _types;
+      }
+
+      public override object[] GetValues()
+      {
+        return new object[] { m_1, m_2, m_3 };
+      }
+    }
+
+    [Serializable, DataContract(Name = "PL", Namespace = "")]
+    private sealed class ParamList<T1, T2, T3, T4> : ParametersListBase
+    {
+      private static readonly Type[] _types = new Type[] { typeof(T1), typeof(T2), typeof(T3), typeof(T4) };
+
+      [DataMember(Name = "A", EmitDefaultValue = false)]
+      private readonly T1 m_1;
+      [DataMember(Name = "B", EmitDefaultValue = false)]
+      private readonly T2 m_2;
+      [DataMember(Name = "C", EmitDefaultValue = false)]
+      private readonly T3 m_3;
+      [DataMember(Name = "D", EmitDefaultValue = false)]
+      private readonly T4 m_4;
+
+      public ParamList(T1 p1, T2 p2, T3 p3, T4 p4)
+      {
+        m_1 = p1;
+        m_2 = p2;
+        m_3 = p3;
+        m_4 = p4;
+      }
+
+      public override Type[] GetTypes()
+      {
+        return _types;
+      }
+
+      public override object[] GetValues()
+      {
+        return new object[] { m_1, m_2, m_3, m_4 };
+      }
+    }
+
+    [Serializable, DataContract(Name = "PL", Namespace = "")]
+    private sealed class ParamList<T1, T2, T3, T4, T5> : ParametersListBase
+    {
+      private static readonly Type[] _types = new Type[] 
+      { 
+        typeof(T1),
+        typeof(T2),
+        typeof(T3),
+        typeof(T4),
+        typeof(T5)
+      };
+
+      [DataMember(Name = "A", EmitDefaultValue = false)]
+      private readonly T1 m_1;
+      [DataMember(Name = "B", EmitDefaultValue = false)]
+      private readonly T2 m_2;
+      [DataMember(Name = "C", EmitDefaultValue = false)]
+      private readonly T3 m_3;
+      [DataMember(Name = "D", EmitDefaultValue = false)]
+      private readonly T4 m_4;
+      [DataMember(Name = "E", EmitDefaultValue = false)]
+      private readonly T5 m_5;
+
+      public ParamList(T1 p1, T2 p2, T3 p3, T4 p4, T5 p5)
+      {
+        m_1 = p1;
+        m_2 = p2;
+        m_3 = p3;
+        m_4 = p4;
+        m_5 = p5;
+      }
+
+      public override Type[] GetTypes()
+      {
+        return _types;
+      }
+
+      public override object[] GetValues()
+      {
+        return new object[] { m_1, m_2, m_3, m_4, m_5 };
+      }
+    }
+
+    [Serializable, DataContract(Name = "PL", Namespace = "")]
+    private sealed class ParamList<T1, T2, T3, T4, T5, T6> : ParametersListBase
+    {
+      private static readonly Type[] _types = new Type[] 
+      { 
+        typeof(T1),
+        typeof(T2),
+        typeof(T3),
+        typeof(T4),
+        typeof(T5),
+        typeof(T6)
+      };
+
+      [DataMember(Name = "A", EmitDefaultValue = false)]
+      private readonly T1 m_1;
+      [DataMember(Name = "B", EmitDefaultValue = false)]
+      private readonly T2 m_2;
+      [DataMember(Name = "C", EmitDefaultValue = false)]
+      private readonly T3 m_3;
+      [DataMember(Name = "D", EmitDefaultValue = false)]
+      private readonly T4 m_4;
+      [DataMember(Name = "E", EmitDefaultValue = false)]
+      private readonly T5 m_5;
+      [DataMember(Name = "F", EmitDefaultValue = false)]
+      private readonly T6 m_6;
+
+      public ParamList(T1 p1, T2 p2, T3 p3, T4 p4, T5 p5, T6 p6)
+      {
+        m_1 = p1;
+        m_2 = p2;
+        m_3 = p3;
+        m_4 = p4;
+        m_5 = p5;
+        m_6 = p6;
+      }
+
+      public override Type[] GetTypes()
+      {
+        return _types;
+      }
+
+      public override object[] GetValues()
+      {
+        return new object[] { m_1, m_2, m_3, m_4, m_5, m_6 };
+      }
+    }
+
+    [Serializable, DataContract(Name = "PL", Namespace = "")]
+    private sealed class ParamList<T1, T2, T3, T4, T5, T6, T7> : ParametersListBase
+    {
+      private static readonly Type[] _types = new Type[] 
+      { 
+        typeof(T1),
+        typeof(T2),
+        typeof(T3),
+        typeof(T4),
+        typeof(T5),
+        typeof(T6),
+        typeof(T7)
+      };
+
+      [DataMember(Name = "A", EmitDefaultValue = false)]
+      private readonly T1 m_1;
+      [DataMember(Name = "B", EmitDefaultValue = false)]
+      private readonly T2 m_2;
+      [DataMember(Name = "C", EmitDefaultValue = false)]
+      private readonly T3 m_3;
+      [DataMember(Name = "D", EmitDefaultValue = false)]
+      private readonly T4 m_4;
+      [DataMember(Name = "E", EmitDefaultValue = false)]
+      private readonly T5 m_5;
+      [DataMember(Name = "F", EmitDefaultValue = false)]
+      private readonly T6 m_6;
+      [DataMember(Name = "G", EmitDefaultValue = false)]
+      private readonly T7 m_7;
+
+      public ParamList(T1 p1, T2 p2, T3 p3, T4 p4, T5 p5, T6 p6, T7 p7)
+      {
+        m_1 = p1;
+        m_2 = p2;
+        m_3 = p3;
+        m_4 = p4;
+        m_5 = p5;
+        m_6 = p6;
+        m_7 = p7;
+      }
+
+      public override Type[] GetTypes()
+      {
+        return _types;
+      }
+
+      public override object[] GetValues()
+      {
+        return new object[] { m_1, m_2, m_3, m_4, m_5, m_6, m_7 };
+      }
+    }
+
+    [Serializable, DataContract(Name = "PL", Namespace = "")]
+    private sealed class ParamList<T1, T2, T3, T4, T5, T6, T7, T8> : ParametersListBase
+    {
+      private static readonly Type[] _types = new Type[] 
+      { 
+        typeof(T1),
+        typeof(T2),
+        typeof(T3),
+        typeof(T4),
+        typeof(T5),
+        typeof(T6),
+        typeof(T7),
+        typeof(T8)
+      };
+
+      [DataMember(Name = "A", EmitDefaultValue = false)]
+      private readonly T1 m_1;
+      [DataMember(Name = "B", EmitDefaultValue = false)]
+      private readonly T2 m_2;
+      [DataMember(Name = "C", EmitDefaultValue = false)]
+      private readonly T3 m_3;
+      [DataMember(Name = "D", EmitDefaultValue = false)]
+      private readonly T4 m_4;
+      [DataMember(Name = "E", EmitDefaultValue = false)]
+      private readonly T5 m_5;
+      [DataMember(Name = "F", EmitDefaultValue = false)]
+      private readonly T6 m_6;
+      [DataMember(Name = "G", EmitDefaultValue = false)]
+      private readonly T7 m_7;
+      [DataMember(Name = "H", EmitDefaultValue = false)]
+      private readonly T8 m_8;
+
+      public ParamList(T1 p1, T2 p2, T3 p3, T4 p4, T5 p5, T6 p6, T7 p7, T8 p8)
+      {
+        m_1 = p1;
+        m_2 = p2;
+        m_3 = p3;
+        m_4 = p4;
+        m_5 = p5;
+        m_6 = p6;
+        m_7 = p7;
+        m_8 = p8;
+      }
+
+      public override Type[] GetTypes()
+      {
+        return _types;
+      }
+
+      public override object[] GetValues()
+      {
+        return new object[] { m_1, m_2, m_3, m_4, m_5, m_6, m_7, m_8 };
+      }
+    }
+  }
+}
---
+++ test/Notung/NotungTest/ParametersListTest.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Notung.Net;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization;
+
+namespace NotungTest
+{
+  [TestClass]
+  public class ParametersListTest
+  {
+    [TestMethod]
+    public void ThreeParameters()
+    {
+      Func<string, float, DateTime, int> func = this.DoSomething;
+
+      var parList = ParametersList.Create(func.Method, "ABBA", 325, DateTime.Now);
+
+      Assert.AreEqual(3, parList.GetTypes().Length);
+      Assert.AreEqual(typeof(string), parList.GetTypes()[0]);
+      Assert.AreEqual(325f, parList.GetValues()[1]);
+    }
+
+    [TestMethod]
+    public void FourParameters()
+    {
+      Func<string, float, DateTime, AddClass, int> func = this.DoSomething;
+
+      var parList = ParametersList.Create(func.Method, "ABBA", 325, DateTime.Now, new AddClass());
+
+      Assert.AreEqual(4, parList.GetTypes().Length);
+      Assert.AreEqual(typeof(string), parList.GetTypes()[0]);
+      Assert.AreEqual(typeof(AddClass), parList.GetTypes()[3]);
+      Assert.AreEqual(325f, parList.GetValues()[1]);
+    }
+
+    [TestMethod]
+    public void BinarySerialization()
+    {
+      Func<string, float, DateTime, AddClass, int> func = this.DoSomething;
+
+      var parList = ParametersList.Create(func.Method, "ABBA", 325, DateTime.Now, new AddClass());
+
+      using (var ms = new MemoryStream())
+      {
+        var bf = new BinaryFormatter();
+        bf.Serialize(ms, parList);
+
+        ms.Position = 0;
+
+        parList = (IParametersList)bf.Deserialize(ms);
+      }
+
+      Assert.AreEqual(4, parList.GetTypes().Length);
+      Assert.AreEqual(typeof(string), parList.GetTypes()[0]);
+      Assert.AreEqual(typeof(AddClass), parList.GetTypes()[3]);
+      Assert.AreEqual(325f, parList.GetValues()[1]);
+    }
+
+    [TestMethod]
+    public void ContractSerialization()
+    {
+      Func<string, float, DateTime, AddClass, int> func = this.DoSomething;
+
+      var parList = ParametersList.Create(func.Method, "ABBA", 325, DateTime.Now, new AddClass());
+      IParametersList parList2 = null;
+
+      using (var ms = new MemoryStream())
+      {
+        var serializer = new DataContractSerializer(ParametersList.GetRequiredType(func.Method));
+
+        serializer.WriteObject(ms, parList);
+        ms.Position = 0;
+
+        var reader = new StreamReader(ms);
+        {
+          var line = reader.ReadToEnd();
+          line.ToLower();
+        }
+
+        ms.Position = 0;
+        serializer = new DataContractSerializer(ParametersList.GetRequiredType(func.Method));
+
+        parList2 = (IParametersList)serializer.ReadObject(ms);
+      }
+
+      Assert.AreEqual(4, parList2.GetTypes().Length);
+      Assert.AreEqual(typeof(string), parList2.GetTypes()[0]);
+      Assert.AreEqual(typeof(AddClass), parList2.GetTypes()[3]);
+      Assert.AreEqual(325f, parList2.GetValues()[1]);
+    }
+
+    [TestMethod]
+    public void BinaryCommandSerizlization()
+    {
+      var method = typeof(IAryx).GetMethod("Sum");
+      ISerializationCommand cmd = new SerializationCommand(typeof(IAryx).GetMethod("Sum"), new object[] { 3, 5 });
+      using (var ms = new MemoryStream())
+      {
+        var ser = new BinaryCallSerializer();
+        ser.Serialize(cmd, ms);
+
+        ms.Position = 0;
+
+        cmd = ser.Deserialize(ms);
+      }
+      Assert.AreEqual(8, cmd.Call(new Aryx()));
+    }
+
+    [TestMethod]
+    public void ContractCommandSerizlization()
+    {
+      var method = typeof(IAryx).GetMethod("Sum");
+      ISerializationCommand cmd = new SerializationCommand(typeof(IAryx).GetMethod("Sum"), new object[] { 3, 5 });
+
+      using (var ms = new MemoryStream())
+      {
+        var ser = new DataContractCallSerializer();
+        ser.Serialize(cmd, ms);
+
+        ms.Position = 0;
+
+        cmd = ser.Deserialize(ms);
+      }
+      Assert.AreEqual(8, cmd.Call(new Aryx()));
+    }
+
+    [TestMethod]
+    public void BinaryCustomCommandSerizlization()
+    {
+      ISerializationCommand cmd = new SomeCommand { Data = 5 };
+      using (var ms = new MemoryStream())
+      {
+        var ser = new BinaryCallSerializer();
+        ser.Serialize(cmd, ms);
+
+        ms.Position = 0;
+
+        cmd = ser.Deserialize(ms);
+      }
+      Assert.AreEqual("{5}", cmd.Call(null));
+    }
+
+    [TestMethod]
+    public void ContractCustomCommandSerizlization()
+    {
+      ISerializationCommand cmd = new SomeCommand { Data = 5 };
+
+      using (var ms = new MemoryStream())
+      {
+        var ser = new DataContractCallSerializer();
+        ser.Serialize(cmd, ms);
+
+        ms.Position = 0;
+
+        cmd = ser.Deserialize(ms);
+      }
+      Assert.AreEqual("{5}", cmd.Call(null));
+    }
+
+    [Serializable, DataContract(Namespace = "")]
+    private class AddClass { }
+
+    private int DoSomething(string name, float price, DateTime date)
+    {
+      return 0;
+    }
+
+    private int DoSomething(string name, float price, DateTime date, AddClass add)
+    {
+      return 0;
+    }
+  }
+
+  public interface IAryx
+  {
+    int Sum(int a, int b);
+
+    int DivMod(int a, int b, out int mod);
+  }
+
+  public class Aryx : IAryx
+  {
+    public int Sum(int a, int b)
+    {
+      return a + b;
+    }
+
+    public int DivMod(int a, int b, out int mod)
+    {
+      var res = a / b;
+
+      mod = a - res * b;
+
+      return res;
+    }
+  }
+
+  [Serializable]
+  public class SomeCommand : ISerializationCommand
+  {
+    private int m_data;
+
+    public int Data 
+    {
+      get {return m_data;}
+      set {m_data = value;}
+    }
+
+    public object Call(object instance)
+    {
+      return "{" + this.Data + "}";
+    }
+  }
+}
