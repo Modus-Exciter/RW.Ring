0660bfc49ac08c6be80f67d02df8d72426fae1a3
Modus-Exciter <glorguin@yandex.ru>
2021-01-03 01:30:12 +0300

    More optimization and refactoring of WeakSet

--- source/Notung/Data/WeakSet.cs
+++ source/Notung/Data/WeakSet.cs
@@ -9,12 +9,11 @@ namespace Notung.Data
   /// Множество элементов, которые могут быть удалены сборщиком мусора
   /// </summary>
   /// <typeparam name="T">Тип элемента множества</typeparam>
-  public class WeakSet <T> : IEnumerable<T> where T : class
+  public class WeakSet<T> : IEnumerable<T> where T : class
   {
     private int m_count = 0;
     private int m_free_index = -1;
     private int m_last_index = 0;
-    private int[] m_buckets;
     private Slot[] m_slots;
     private readonly SharedLock m_lock = new SharedLock(false);
 
@@ -22,6 +21,7 @@ namespace Notung.Data
     /// Добавление нового элемента в множество
     /// </summary>
     /// <param name="item">Добавляемый элемент</param>
+    /// <returns>True, если элемент был добавлен в множество. False, если такой элемент уже был в множестве</returns>
     public bool Add(T item)
     {
       if (item == null)
@@ -29,17 +29,18 @@ namespace Notung.Data
 
       using (m_lock.WriteLock())
       {
-        if (m_buckets == null)
-          this.Initialize(0);
+        if (m_slots == null)
+          m_slots = new Slot[3];
 
         int previous;
-        int index;
-        int slot_index = this.FindIndex(item, out index, out previous, true);
+        int bucket_index;
+        int slot_index = this.FindSlotIndex(item, out bucket_index, out previous, true);
 
         if (slot_index >= 0)
           return false;
 
-        this.AddSlot(item, index, slot_index);
+        this.AddSlot(item, bucket_index, slot_index);
+
         return true;
       }
     }
@@ -56,7 +57,7 @@ namespace Notung.Data
 
       using (m_lock.ReadLock())
       {
-        return this.FindIndex(item, out index, out previous, false) >= 0;
+        return this.FindSlotIndex(item, out index, out previous, false) >= 0;
       }
     }
 
@@ -68,17 +69,17 @@ namespace Notung.Data
     public bool Remove(T item)
     {
       int previous;
-      int index;
+      int bucket_index;
 
       using (m_lock.WriteLock())
       {
-        var slot = this.FindIndex(item, out index, out previous, true);
+        var slot_index = this.FindSlotIndex(item, out bucket_index, out previous, true);
 
-        if (slot < 0)
+        if (slot_index < 0)
           return false;
         else
         {
-          this.RemoveSlot(index, slot, previous);
+          this.RemoveSlot(bucket_index, slot_index, previous);
           return true;
         }
       }
@@ -98,7 +99,6 @@ namespace Notung.Data
         }
 
         m_slots = null;
-        m_buckets = null;
         m_count = 0;
         m_free_index = -1;
         m_last_index = 0;
@@ -130,20 +130,11 @@ namespace Notung.Data
 
     #region Implementation ------------------------------------------------------------------------
 
-    private void Initialize(int capacity)
-    {
-      int prime = PrimeHelper.GetPrime(capacity);
-
-      m_buckets = new int[prime];
-      m_slots = new Slot[prime];
-    }
-
     private void IncreaseCapacity()
     {
       int min = this.m_count * 2;
       int prime = PrimeHelper.GetPrime(min < 0 ? m_count : min);
       Slot[] tmp_slots = new Slot[prime];
-      int[] tmp_buckets = new int[prime];
 
       if (m_slots != null)
         Array.Copy(m_slots, 0, tmp_slots, 0, m_last_index);
@@ -151,25 +142,24 @@ namespace Notung.Data
       for (int i = 0; i < m_last_index; i++)
       {
         int index = (tmp_slots[i].hashCode & int.MaxValue) % prime;
-        tmp_slots[i].next = tmp_buckets[index] - 1;
-        tmp_buckets[index] = i + 1;
+        tmp_slots[i].next = tmp_slots[index].bucket - 1;
+        tmp_slots[index].bucket = i + 1;
       }
 
       m_slots = tmp_slots;
-      m_buckets = tmp_buckets;
     }
 
-    private int FindIndex(T item, out int index, out int previous, bool removeExpired)
+    private int FindSlotIndex(T item, out int bucketIndex, out int previous, bool removeExpired)
     {
       if (item == null)
         throw new ArgumentNullException("item");
 
-      var hash_code = item.GetHashCode() ;
+      var hash_code = item.GetHashCode();
 
       previous = -1;
-      index = (hash_code & int.MaxValue) % m_buckets.Length;
+      bucketIndex = (hash_code & int.MaxValue) % m_slots.Length;
 
-      for (int i = this.m_buckets[index] - 1; i >= 0; i = this.m_slots[i].next)
+      for (int i = m_slots[bucketIndex].bucket - 1; i >= 0; i = m_slots[i].next)
       {
         if (m_slots[i].hashCode == hash_code)
         {
@@ -181,7 +171,7 @@ namespace Notung.Data
               return i;
           }
           else if (removeExpired)
-            this.RemoveSlot(index, i, previous);
+            this.RemoveSlot(bucketIndex, i, previous);
         }
 
         previous = i;
@@ -201,10 +191,10 @@ namespace Notung.Data
       }
       else
       {
-        if (m_last_index == this.m_slots.Length)
+        if (m_last_index == m_slots.Length)
         {
           this.IncreaseCapacity();
-          bucketIndex = (hash_code & int.MaxValue) % m_buckets.Length;
+          bucketIndex = (hash_code & int.MaxValue) % m_slots.Length;
         }
 
         slotIndex = m_last_index;
@@ -213,8 +203,8 @@ namespace Notung.Data
 
       m_slots[slotIndex].hashCode = hash_code;
       m_slots[slotIndex].handle = GCHandle.Alloc(item, GCHandleType.Weak);
-      m_slots[slotIndex].next = m_buckets[bucketIndex] - 1;
-      m_buckets[bucketIndex] = slotIndex + 1;
+      m_slots[slotIndex].next = m_slots[bucketIndex].bucket - 1;
+      m_slots[bucketIndex].bucket = slotIndex + 1;
       m_count++;
     }
 
@@ -224,9 +214,9 @@ namespace Notung.Data
         m_slots[slotIndex].handle.Free();
 
       if (previous < 0)
-        m_buckets[bucketIndex] = m_slots[slotIndex].next + 1;
+        m_slots[bucketIndex].bucket = m_slots[slotIndex].next + 1;
       else
-        m_slots[previous].next = this.m_slots[slotIndex].next;
+        m_slots[previous].next = m_slots[slotIndex].next;
 
       m_slots[slotIndex].hashCode = -1;
       m_slots[slotIndex].handle = default(GCHandle);
@@ -246,6 +236,7 @@ namespace Notung.Data
     {
       public int hashCode;
       public int next;
+      public int bucket;
       public GCHandle handle;
 
       public T Target
@@ -261,7 +252,16 @@ namespace Notung.Data
   {
     public static bool IsPrime(int value)
     {
-      for (int i = 2; i <= value / i; i++)
+      if (value < 2)
+        return false;
+      else if (value == 2)
+        return true;
+      else if (value % 2 == 0)
+        return false;
+
+      var sqrt = (int)Math.Sqrt(value);
+
+      for (int i = 3; i <= sqrt; i += 2)
       {
         if (value % i == 0)
           return false;
@@ -293,7 +293,7 @@ namespace Notung.Data
           even = !even;
         }
       }
-      
+
       return value;
     }
   }
