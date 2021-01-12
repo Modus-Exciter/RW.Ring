eab76d1c0a3c8fb8ff46f500b96f97d0dafc1b20
Modus <glorguin@yandex.ru>
2021-01-12 18:58:00 +0300

    Import from generic excel file instead of WOFOST-specific Excel table format.

---
+++ source/ImportWofostResults/ExcelImportForm.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace JetExcelOleDbImport
+{
+  public partial class ExcelImportForm : Form
+  {
+    public ExcelImportForm()
+    {
+      InitializeComponent();
+    }
+
+    public string ExcelFileName
+    {
+      get { return m_path_edit.Text; }
+    }
+
+    public string TableName
+    {
+      get { return m_table_edit.SelectedItem as string; }
+    }
+
+    private void m_button_open_excel_Click(object sender, EventArgs e)
+    {
+      using (var dlg = new OpenFileDialog())
+      {
+        dlg.Filter = "Excel files|*.xls";
+
+        if (dlg.ShowDialog(this) == DialogResult.OK)
+        {
+          m_path_edit.Text = dlg.FileName;
+
+          var mask = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=\"{0}\";Extended Properties=\"Excel 8.0;HDR=YES;IMEX=1\"";
+
+          using (var ole_cn = new OleDbConnection(string.Format(mask, m_path_edit.Text)))
+          {
+            ole_cn.Open();
+            DataTable tables = ole_cn.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null);
+
+            foreach (DataRow row in tables.Rows)
+              m_table_edit.Items.Add(row["TABLE_NAME"].ToString());
+          }
+        }
+      }
+    }
+
+    protected override void OnFormClosing(FormClosingEventArgs e)
+    {
+      base.OnFormClosing(e);
+
+      if (this.DialogResult != DialogResult.OK)
+        return;
+
+      List<string> messages = new List<string>();
+
+      if (string.IsNullOrEmpty(m_path_edit.Text))
+        messages.Add("Не выбран файл Excel");
+      if (string.IsNullOrEmpty(this.TableName))
+        messages.Add("Не выбрана таблица");
+
+      if (messages.Count > 0)
+      {
+        MessageBox.Show(string.Join(Environment.NewLine, messages), this.Text);
+        e.Cancel = true;
+      }
+    }
+  }
+}
---
+++ source/ImportWofostResults/ImportExcel.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.IO;
+using System.Windows.Forms;
+using Schicksal.Exchange;
+
+namespace JetExcelOleDbImport
+{
+  public class ImportExcel : MarshalByRefObject, ITableImport
+  {
+    public ImportResult Import(object context)
+    {
+      using (var dlg = new ExcelImportForm())
+      {
+        if (dlg.ShowDialog() == DialogResult.OK)
+        {
+          var mask = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=\"{0}\";Extended Properties=\"Excel 8.0;HDR=YES;IMEX=1\"";
+
+          using (var connection = new OleDbConnection(string.Format(mask, dlg.ExcelFileName)))
+          {
+            connection.Open();
+
+            var cmd = connection.CreateCommand();
+            cmd.CommandText = dlg.TableName;
+            cmd.CommandType = CommandType.TableDirect;
+
+            var table = new DataTable();
+
+            using (var reader = cmd.ExecuteReader())
+              table.Load(reader);
+
+            table = NormalizeTable(table);
+
+            return new ImportResult
+            {
+              Table = table,
+              Description = string.Format("Import from {0}", Path.GetFileName(dlg.ExcelFileName))
+            };
+          }
+        }
+      }
+
+      return null;
+    }
+
+    private static DataTable NormalizeTable(DataTable table)
+    {
+      HashSet<string> nulls = new HashSet<string>();
+      HashSet<string> unconvertable = new HashSet<string>();
+
+      foreach (DataRow row in table.Rows)
+      {
+        foreach (DataColumn col in table.Columns)
+        {
+          if (row.IsNull(col))
+            nulls.Add(col.ColumnName);
+          else
+          {
+            if (col.DataType == typeof(float))
+            {
+              var fl = (float)row[col];
+
+              if (fl != (int)fl)
+                unconvertable.Add(col.ColumnName);
+            }
+            if (col.DataType == typeof(double))
+            {
+              var fl = (double)row[col];
+
+              if (fl != (int)fl)
+                unconvertable.Add(col.ColumnName);
+            }
+          }
+        }
+      }
+
+      var copy = table.Clone();
+
+      foreach (DataColumn col in copy.Columns)
+      {
+        if (nulls.Contains(col.ColumnName))
+          continue;
+
+        if (col.DataType == typeof(string))
+          col.DefaultValue = string.Empty;
+
+        if (col.DataType == typeof(double) || col.DataType == typeof(float))
+        {
+          if (!unconvertable.Contains(col.ColumnName))
+            col.DataType = typeof(int);
+        }
+
+        if (col.DataType != typeof(double) && col.DataType != typeof(float))
+          col.AllowDBNull = false;
+      }
+
+      copy.Load(table.CreateDataReader());
+      return copy;
+    }
+
+    public override string ToString()
+    {
+      return "Импорт данных из Excel";
+    }
+  }
+}
