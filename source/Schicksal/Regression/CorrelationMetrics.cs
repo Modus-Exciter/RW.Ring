9b2624a66f79b57e748defae30b5c6ba6c8c1fce
Lab_unit <glorguin@yandex.ru>
2021-07-19 11:40:33 +0300

    Begin of regression analysis

---
+++ source/Schicksal/Regression/CorrelationMetrics.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Schicksal.Regression
+{
+  public sealed class CorrelationMetrics
+  {
+    public double R { get; set; }
+
+    public double Z { get; set; }
+  }
+}
