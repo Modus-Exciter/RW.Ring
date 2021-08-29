3cff55ab94cd0d6ff4e2060a723a2a71132bfe89
Modus <glorguin@yandex.ru>
2021-08-30 00:27:30 +0300

    Added log record parsing algorithm to LogStringBuilder.

--- source/Notung/Logging/LogStringBuilder.cs
+++ source/Notung/Logging/LogStringBuilder.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Globalization;
 using System.IO;
 using System.Linq;
+using Notung.Properties;
 using Notung.Threading;
 
 namespace Notung.Logging
 {
-  public class LogStringBuilder
+  /// <summary>
+  /// Преобразование сообщений лога в текст и обратно
+  /// </summary>
+  public sealed class LogStringBuilder
   {
     private readonly IBuildBlock[] m_blocks;
     private readonly ThreadField<char[]> m_date_converter = new ThreadField<char[]>();
@@ -14,6 +20,12 @@ namespace Notung.Logging
 
     private static readonly int _pid = Global.CurrentProcess.Id;
 
+    private const int DATE_CONVERTER_SIZE = 23;
+
+    /// <summary>
+    /// Инициализация преобразования на основе шаблона
+    /// </summary>
+    /// <param name="template">Шаблон сообщения лога</param>
     public LogStringBuilder(string template)
     {
       if (template == null)
@@ -22,6 +34,11 @@ namespace Notung.Logging
       m_blocks = StateMachine(template).ToArray();
     }
 
+    /// <summary>
+    /// Преобразование сообщения логгирования в текст
+    /// </summary>
+    /// <param name="writer">Поток, в который нужно записать текст</param>
+    /// <param name="data">Сообщение логгирования</param>
     public void BuildString(TextWriter writer, LoggingEvent data)
     {
 #if DEBUG
@@ -38,6 +55,55 @@ namespace Notung.Logging
         writer.WriteLine();
         writer.Write(data.Data);
       }
+    }
+
+    /// <summary>
+    /// Заполнение строки таблицы данными из текста сообщения логгиирования
+    /// </summary>
+    /// <param name="rawText">Сообщение логгирования, преобразованное в текст</param>
+    /// <param name="table">Таблица, в которой требуется создать строку</param>
+    public void FillRow(string rawText, DataTable table)
+    {
+      int[] token_starts = this.GetTokenStarts(rawText);
+
+      if (table.Rows.Count == 0)
+        this.CreateTableColumns(table);
+
+      var row = table.NewRow();
+
+      for (int i = 0; i < token_starts.Length; i++)
+      {
+        var field = m_blocks[i] as FieldBlock;
+        if (field != null)
+        {
+          string token = i < token_starts.Length - 1 ?
+            rawText.Substring(token_starts[i], token_starts[i + 1] - token_starts[i]) :
+            rawText.Substring(token_starts[i]);
+
+          switch (field.Type)
+          {
+            case FieldType.Date:
+              FormatFieldBlock format = field as FormatFieldBlock;
+
+              if (format != null && !string.IsNullOrEmpty(format.Format))
+                row[field.m_field] = DateTime.ParseExact(token, format.Format, CultureInfo.InvariantCulture);
+              else
+                row[field.m_field] = DateTime.ParseExact(token, "dd.MM.yyyy HH:mm:ss.fff", CultureInfo.InvariantCulture);
+
+              break;
+
+            case FieldType.Process:
+              row[field.m_field] = int.Parse(token);
+              break;
+
+            default:
+              row[field.m_field] = token;
+              break;
+          }
+        }
+      }
+
+      table.Rows.Add(row);
     }
 
     public override string ToString()
@@ -60,13 +126,15 @@ namespace Notung.Logging
         {
           if (template[i] == '\\')
           {
-            blocks.Add(new TokenBlock(template.Substring(string_start, i - string_start)));
+            if (string_start < i)
+              blocks.Add(new TokenBlock(template.Substring(string_start, i - string_start)));
             state = State.Escape;
             string_start = i + 1;
           }
           else if (template[i] == '{')
           {
-            blocks.Add(new TokenBlock(template.Substring(string_start, i - string_start)));
+            if (string_start < i)
+              blocks.Add(new TokenBlock(template.Substring(string_start, i - string_start)));
             string_start = i + 1;
             state = State.Bracket;
           }
@@ -121,7 +189,7 @@ namespace Notung.Logging
     {
       if (m_date_converter.Instance == null)
       {
-        var array = new char[23];
+        var array = new char[DATE_CONVERTER_SIZE];
 
         array[2] = '.';
         array[5] = '.';
@@ -131,6 +199,111 @@ namespace Notung.Logging
         array[19] = '.';
 
         m_date_converter.Instance = array;
+      }
+    }
+
+    private int[] GetTokenStarts(string rawText)
+    {
+      int[] token_starts = new int[m_blocks.Length];
+      int start = 0;
+
+      for (int i = 0; i < m_blocks.Length; i++)
+      {
+        var token = m_blocks[i] as TokenBlock;
+
+        if (token != null)
+        {
+          start = rawText.IndexOf(token.Token, start);
+
+          if (start >= 0)
+          {
+            token_starts[i] = start;
+            start += token.Token.Length;
+
+            if (i < m_blocks.Length - 1)
+              token_starts[i + 1] = start;
+          }
+          else
+            throw new FormatException(Resources.LOG_PARSE_FAIL);
+        }
+      }
+
+      for (int i = 1; i < token_starts.Length; i++)
+      {
+        if (token_starts[i] > 0)
+          continue;
+
+        var block = m_blocks[i - 1] as FieldBlock;
+
+        if (block != null)
+          token_starts[i] = token_starts[i - 1] + this.GetTokenLength(rawText, block, i - 1, token_starts);
+      }
+
+      return token_starts;
+    }
+
+    private int GetTokenLength(string rawText, FieldBlock block, int index, int[] token_starts)
+    {
+      switch (block.Type)
+      {
+        case FieldType.Level:
+          var level = Enum.GetValues(typeof(InfoLevel)).Cast<InfoLevel>().Select(l =>
+            l.ToString()).SingleOrDefault(l => rawText.IndexOf(l, token_starts[index]) > 0);
+
+          if (level != null)
+            return level.Length;
+          else
+            throw new FormatException(string.Format(Resources.ENUM_PARSE_FAIL, typeof(InfoLevel)));
+
+        case FieldType.Date:
+          FormatFieldBlock format = block as FormatFieldBlock;
+
+          if (format != null && !string.IsNullOrEmpty(format.Format))
+            return DateTime.Now.ToString(format.Format).Length;
+          else
+            return DATE_CONVERTER_SIZE;
+
+        case FieldType.Process:
+          var x = token_starts[index];
+
+          while (char.IsDigit(rawText[x]))
+            x++;
+
+          return x - token_starts[index];
+
+        default:
+          throw new InvalidExpressionException(string.Format(Resources.NO_TOKEN_LENGTH, block.Type));
+      }
+    }
+
+    private void CreateTableColumns(DataTable table)
+    {
+      for (int i = 0; i < m_blocks.Length; i++)
+      {
+        var field = m_blocks[i] as FieldBlock;
+
+        if (field != null)
+        {
+          if (table.Columns.Contains(field.m_field))
+            continue;
+
+          var column = table.Columns.Add(field.m_field);
+
+          switch (field.Type)
+          {
+            case FieldType.Date:
+              column.DataType = typeof(DateTime);
+              break;
+
+            case FieldType.Process:
+              column.DataType = typeof(int);
+              break;
+
+            default:
+              column.DataType = typeof(string);
+              break;
+          }
+        }
       }
     }
 
@@ -174,6 +347,11 @@ namespace Notung.Logging
         m_token = token;
       }
 
+      public string Token
+      {
+        get { return m_token; }
+      }
+
       public void Build(TextWriter writer, LoggingEvent data)
       {
         writer.Write(m_token);
@@ -190,7 +368,7 @@ namespace Notung.Logging
  
     private class FieldBlock : IBuildBlock
     {
-      protected readonly string m_field;
+      public readonly string m_field;
       public readonly FieldType Type;
       private readonly ThreadField<char[]> m_date_converter;
 
@@ -360,10 +538,10 @@ namespace Notung.Logging
 
       public override string ToString()
       {
-        if (!string.IsNullOrEmpty(Format))
-          return "{" + m_field + ':' + Format + "}";
-        else
+        if (string.IsNullOrEmpty(Format))
           return "{" + m_field + "}";
+        else
+          return "{" + m_field + ':' + Format + "}";
       }
     }
 
--- test/Notung/NotungTest/LoggingTest.cs
+++ test/Notung/NotungTest/LoggingTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data;
 using System.Diagnostics;
 using System.IO;
 using System.Text;
@@ -59,9 +60,75 @@ namespace NotungTest
     public void LogStringBuilderTest()
     {
       var bldr = new LogStringBuilder("Summa \\{1} and {SUKA} and {RUKA: 67612} RRR");
-      var bldr2 = new LogStringBuilder("Summa \\{1} and {SUKA} and {RUKA: 67612} RRR{Message}");
+      var bldr2 = new LogStringBuilder("Summa \\{1} and {SUKA} and {RUKA: 67612} RRR{Message}!!");
 
-      Assert.AreEqual("Summa \\{1} and {SUKA} and {RUKA: 67612} RRR{Message}", bldr2.ToString());
+      Assert.AreEqual("Summa \\{1} and {SUKA} and {RUKA: 67612} RRR{Message}!!", bldr2.ToString());
+    }
+
+    [TestMethod]
+    public void LogStringBuilderFill()
+    {
+      var bldr = new LogStringBuilder(@"[{Date}] [{Level}] [{Process}] [{Source}]
+{Message}");
+
+      DataTable table = new DataTable();
+
+      bldr.FillRow(@"[10.08.2021 15:36:48.266] [Debug] [10256] [Notung.Configuration.ConfigurationFile]
+Save(): C: \Users\Modus\AppData\Local\ARI\Schicksal\1.0.1.0\settings.config", table);
+
+      Assert.AreEqual(1, table.Rows.Count);
+      Assert.AreEqual(5, table.Columns.Count);
+      Assert.AreEqual(new DateTime(2021, 8, 10, 15, 36, 48, 266), table.Rows[0]["Date"]);
+      Assert.AreEqual("Debug", table.Rows[0]["Level"]);
+      Assert.AreEqual(10256, table.Rows[0]["Process"]);
+      Assert.AreEqual("Notung.Configuration.ConfigurationFile", table.Rows[0]["Source"]);
+      Assert.AreEqual(@"Save(): C: \Users\Modus\AppData\Local\ARI\Schicksal\1.0.1.0\settings.config", table.Rows[0]["Message"]);
+    }
+
+    [TestMethod]
+    public void LogStringBuilderTokenFix()
+    {
+      var bldr = new LogStringBuilder("[{Date}{Process}{Level}{Message}].[{Source}]");
+
+      DataTable table = new DataTable();
+      bldr.FillRow(@"[10.08.2021 15:36:48.26610256DebugSave(): C: \Users\Modus\AppData\Local\ARI\Schicksal\1.0.1.0\settings.config].[Notung.Configuration.ConfigurationFile]", table);
+      Assert.AreEqual(1, table.Rows.Count);
+      Assert.AreEqual(5, table.Columns.Count);
+      Assert.AreEqual(new DateTime(2021, 8, 10, 15, 36, 48, 266), table.Rows[0]["Date"]);
+      Assert.AreEqual("Debug", table.Rows[0]["Level"]);
+      Assert.AreEqual(10256, table.Rows[0]["Process"]);
+      Assert.AreEqual("Notung.Configuration.ConfigurationFile", table.Rows[0]["Source"]);
+      Assert.AreEqual(@"Save(): C: \Users\Modus\AppData\Local\ARI\Schicksal\1.0.1.0\settings.config", table.Rows[0]["Message"]);
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(FormatException))]
+    public void LogStringBuilderEnumFail()
+    {
+      var bldr = new LogStringBuilder("[{Date}{Process}{Level}{Message}].[{Source}]");
+
+      DataTable table = new DataTable();
+      bldr.FillRow(@"[10.08.2021 15:36:48.26610256DibugSave(): C: \Users\Modus\AppData\Local\ARI\Schicksal\1.0.1.0\settings.config].[Notung.Configuration.ConfigurationFile]", table);
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(FormatException))]
+    public void LogStringBuilderFormatMismatch()
+    {
+      var bldr = new LogStringBuilder("[{Date}{Process}{Level}{Message}].[{Source}].");
+
+      DataTable table = new DataTable();
+      bldr.FillRow(@"[10.08.2021 15:36:48.26610256DebugSave(): C: \Users\Modus\AppData\Local\ARI\Schicksal\1.0.1.0\settings.config].[Notung.Configuration.ConfigurationFile]", table);
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(InvalidExpressionException))]
+    public void LogStringBuilderTokenTypeFail()
+    {
+      var bldr = new LogStringBuilder("[{Message}{Source}]");
+
+      DataTable table = new DataTable();
+      bldr.FillRow(@"[Save(): C: \Users\Modus\AppData\Local\ARI\Schicksal\1.0.1.0\settings.config].[Notung.Configuration.ConfigurationFile]", table);
     }
 
     [TestMethod]
