e49cd7b0264b337734017bb92dd7a8c1808efc65
Modus <glorguin@yandex.ru>
2021-09-19 20:41:39 +0300

    Added focus on the tree while selecting folder

--- source/Notung/ComponentModel/ObservableObject.cs
+++ source/Notung/ComponentModel/ObservableObject.cs
@@ -1,29 +1,42 @@
 ﻿using System;
 using System.Collections.Concurrent;
 using System.ComponentModel;
+using System.Diagnostics;
 
 namespace Notung.ComponentModel
 {
   public abstract class ObservableObject : INotifyPropertyChanged
   {
-    private static readonly ConcurrentDictionary<string, PropertyChangedEventArgs> _property_hash =
+    private static readonly ConcurrentDictionary<string, PropertyChangedEventArgs> _property_cache =
       new ConcurrentDictionary<string, PropertyChangedEventArgs>();
 
-    private static readonly Func<string, PropertyChangedEventArgs> _creator =
-      name => new PropertyChangedEventArgs(name);
+    private static readonly ConcurrentDictionary<Type, EventArgsCreator> _type_cache =
+      new ConcurrentDictionary<Type, EventArgsCreator>();
+
+    private static readonly Func<Type, EventArgsCreator> _creator =
+      type => new EventArgsCreator(type);
 
     private static readonly PropertyChangedEventArgs _all_properties =
       new PropertyChangedEventArgs(null);
-    
+
+    private readonly EventArgsCreator m_creator;
+
+    protected ObservableObject()
+    {
+      m_creator = _type_cache.GetOrAdd(this.GetType(), _creator);
+    }
+
     public event PropertyChangedEventHandler PropertyChanged;
 
     protected void OnPropertyChanged(string propertyName)
     {
-      if (string.IsNullOrEmpty(propertyName))
-        this.PropertyChanged.InvokeIfSubscribed(this, _all_properties);
-      else
+      if (!string.IsNullOrEmpty(propertyName))
+      {
         this.PropertyChanged.InvokeIfSubscribed(this,
-          _property_hash.GetOrAdd(propertyName, _creator));
+          _property_cache.GetOrAdd(propertyName, m_creator.Create));
+      }
+      else
+        this.PropertyChanged.InvokeIfSubscribed(this, _all_properties);
     }
 
     protected void ChangeValue<T>(ref T field, T value, string propertyName)
@@ -39,6 +52,24 @@ namespace Notung.ComponentModel
       field = value;
 
       this.OnPropertyChanged(propertyName);
+    }
+
+    private class EventArgsCreator
+    {
+      private readonly PropertyDescriptorCollection m_properties;
+      public readonly Func<string, PropertyChangedEventArgs> Create;
+
+      public EventArgsCreator(Type type)
+      {
+        m_properties = TypeDescriptor.GetProperties(type);
+        Create = propertyName =>
+        {
+          Debug.Assert(m_properties[propertyName] != null,
+            string.Format("Property {0} not found", propertyName));
+
+          return new PropertyChangedEventArgs(propertyName);
+        };
+      }
     }
   }
 }
