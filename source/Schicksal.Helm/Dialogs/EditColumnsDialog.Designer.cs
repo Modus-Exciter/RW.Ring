57e29d1456d1b12732b8ae29d523e4d7faf95608
Modus <glorguin@yandex.ru>
2022-11-20 14:24:59 +0300

    Added MST algorithms

--- source/Schicksal.Helm/Dialogs/EditColumnsDialog.Designer.cs
+++ source/Schicksal.Helm/Dialogs/EditColumnsDialog.Designer.cs
@@ -28,90 +28,90 @@
     /// </summary>
     private void InitializeComponent()
     {
-      this.components = new System.ComponentModel.Container();
-      System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(EditColumnsDialog));
-      this.m_bottom_panel = new System.Windows.Forms.FlowLayoutPanel();
-      this.m_button_cancel = new System.Windows.Forms.Button();
-      this.m_button_ok = new System.Windows.Forms.Button();
-      this.m_grid = new System.Windows.Forms.DataGridView();
-      this.m_name_column = new System.Windows.Forms.DataGridViewTextBoxColumn();
-      this.m_type_column = new System.Windows.Forms.DataGridViewComboBoxColumn();
-      this.m_binding_source = new System.Windows.Forms.BindingSource(this.components);
-      this.m_bottom_panel.SuspendLayout();
-      ((System.ComponentModel.ISupportInitialize)(this.m_grid)).BeginInit();
-      ((System.ComponentModel.ISupportInitialize)(this.m_binding_source)).BeginInit();
-      this.SuspendLayout();
-      // 
-      // m_bottom_panel
-      // 
-      resources.ApplyResources(this.m_bottom_panel, "m_bottom_panel");
-      this.m_bottom_panel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-      this.m_bottom_panel.Controls.Add(this.m_button_cancel);
-      this.m_bottom_panel.Controls.Add(this.m_button_ok);
-      this.m_bottom_panel.Name = "m_bottom_panel";
-      // 
-      // m_button_cancel
-      // 
-      resources.ApplyResources(this.m_button_cancel, "m_button_cancel");
-      this.m_button_cancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-      this.m_button_cancel.Name = "m_button_cancel";
-      this.m_button_cancel.UseVisualStyleBackColor = true;
-      // 
-      // m_button_ok
-      // 
-      resources.ApplyResources(this.m_button_ok, "m_button_ok");
-      this.m_button_ok.DialogResult = System.Windows.Forms.DialogResult.OK;
-      this.m_button_ok.Name = "m_button_ok";
-      this.m_button_ok.UseVisualStyleBackColor = true;
-      // 
-      // m_grid
-      // 
-      this.m_grid.AutoGenerateColumns = false;
-      this.m_grid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-      this.m_grid.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(EditColumnsDialog));
+            this.m_bottom_panel = new System.Windows.Forms.FlowLayoutPanel();
+            this.m_button_cancel = new System.Windows.Forms.Button();
+            this.m_button_ok = new System.Windows.Forms.Button();
+            this.m_grid = new System.Windows.Forms.DataGridView();
+            this.m_name_column = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.m_type_column = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            this.m_binding_source = new System.Windows.Forms.BindingSource(this.components);
+            this.m_bottom_panel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.m_grid)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.m_binding_source)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // m_bottom_panel
+            // 
+            resources.ApplyResources(this.m_bottom_panel, "m_bottom_panel");
+            this.m_bottom_panel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.m_bottom_panel.Controls.Add(this.m_button_cancel);
+            this.m_bottom_panel.Controls.Add(this.m_button_ok);
+            this.m_bottom_panel.Name = "m_bottom_panel";
+            // 
+            // m_button_cancel
+            // 
+            resources.ApplyResources(this.m_button_cancel, "m_button_cancel");
+            this.m_button_cancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.m_button_cancel.Name = "m_button_cancel";
+            this.m_button_cancel.UseVisualStyleBackColor = true;
+            // 
+            // m_button_ok
+            // 
+            resources.ApplyResources(this.m_button_ok, "m_button_ok");
+            this.m_button_ok.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.m_button_ok.Name = "m_button_ok";
+            this.m_button_ok.UseVisualStyleBackColor = true;
+            // 
+            // m_grid
+            // 
+            this.m_grid.AutoGenerateColumns = false;
+            this.m_grid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.m_grid.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.m_name_column,
             this.m_type_column});
-      this.m_grid.DataSource = this.m_binding_source;
-      resources.ApplyResources(this.m_grid, "m_grid");
-      this.m_grid.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
-      this.m_grid.Name = "m_grid";
-      this.m_grid.CellEnter += new System.Windows.Forms.DataGridViewCellEventHandler(this.m_grid_CellEnter);
-      this.m_grid.CellMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.m_grid_CellMouseClick);
-      // 
-      // m_name_column
-      // 
-      this.m_name_column.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-      this.m_name_column.DataPropertyName = "ColumnName";
-      resources.ApplyResources(this.m_name_column, "m_name_column");
-      this.m_name_column.Name = "m_name_column";
-      // 
-      // m_type_column
-      // 
-      this.m_type_column.DataPropertyName = "ColumnType";
-      resources.ApplyResources(this.m_type_column, "m_type_column");
-      this.m_type_column.MaxDropDownItems = 12;
-      this.m_type_column.Name = "m_type_column";
-      // 
-      // m_binding_source
-      // 
-      this.m_binding_source.DataSource = typeof(Schicksal.Helm.Dialogs.TableColumnInfo);
-      // 
-      // EditColumnsDialog
-      // 
-      this.AcceptButton = this.m_button_ok;
-      resources.ApplyResources(this, "$this");
-      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-      this.CancelButton = this.m_button_cancel;
-      this.Controls.Add(this.m_grid);
-      this.Controls.Add(this.m_bottom_panel);
-      this.Name = "EditColumnsDialog";
-      this.ShowInTaskbar = false;
-      this.m_bottom_panel.ResumeLayout(false);
-      this.m_bottom_panel.PerformLayout();
-      ((System.ComponentModel.ISupportInitialize)(this.m_grid)).EndInit();
-      ((System.ComponentModel.ISupportInitialize)(this.m_binding_source)).EndInit();
-      this.ResumeLayout(false);
-      this.PerformLayout();
+            this.m_grid.DataSource = this.m_binding_source;
+            resources.ApplyResources(this.m_grid, "m_grid");
+            this.m_grid.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.m_grid.Name = "m_grid";
+            this.m_grid.CellEnter += new System.Windows.Forms.DataGridViewCellEventHandler(this.m_grid_CellEnter);
+            this.m_grid.CellMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.m_grid_CellMouseClick);
+            // 
+            // m_name_column
+            // 
+            this.m_name_column.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.m_name_column.DataPropertyName = "ColumnName";
+            resources.ApplyResources(this.m_name_column, "m_name_column");
+            this.m_name_column.Name = "m_name_column";
+            // 
+            // m_type_column
+            // 
+            this.m_type_column.DataPropertyName = "ColumnType";
+            resources.ApplyResources(this.m_type_column, "m_type_column");
+            this.m_type_column.MaxDropDownItems = 12;
+            this.m_type_column.Name = "m_type_column";
+            // 
+            // m_binding_source
+            // 
+            this.m_binding_source.DataSource = typeof(Schicksal.Helm.Dialogs.TableColumnInfo);
+            // 
+            // EditColumnsDialog
+            // 
+            this.AcceptButton = this.m_button_ok;
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.m_button_cancel;
+            this.Controls.Add(this.m_grid);
+            this.Controls.Add(this.m_bottom_panel);
+            this.Name = "EditColumnsDialog";
+            this.ShowInTaskbar = false;
+            this.m_bottom_panel.ResumeLayout(false);
+            this.m_bottom_panel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.m_grid)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.m_binding_source)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
     }
 
