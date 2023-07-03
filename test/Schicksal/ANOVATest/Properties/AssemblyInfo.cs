81717a988420ba8ed6397fd1bb134228e47e512c
kradunches <sasha10131310@gmail.com>
2023-07-03 20:39:57 +0300

    One ANOVA test added

---
+++ test/Schicksal/ANOVATest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ANOVATest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ANOVATest")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c2da15be-3aa7-498d-b899-41e0743c20fa")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ test/Schicksal/ANOVATest/UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Schicksal.Anova;
+using Schicksal.Basic;
+
+namespace ANOVATest
+{
+  [TestClass]
+  public class UnitTest1
+  {
+    [TestMethod]
+    public void TestMethod1()
+    {
+      ArrayDataGroup groupA1 = new ArrayDataGroup(new double[] { 48, 49, 50 });
+      ArrayDataGroup groupA2 = new ArrayDataGroup(new double[] { 53, 54, 55 });
+      MultiArrayDataGroup multiA = new MultiArrayDataGroup(new IDataGroup[] { groupA1, groupA2 });
+
+      ArrayDataGroup groupB1 = new ArrayDataGroup(new double[] { 18, 19, 20 });
+      ArrayDataGroup groupB2 = new ArrayDataGroup(new double[] { 24, 25, 26 });
+      MultiArrayDataGroup multiB = new MultiArrayDataGroup(new IDataGroup[] { groupB1, groupB2 });
+
+      SetMultiArrayDataGroup set = new SetMultiArrayDataGroup(new IMultyDataGroup[] { multiA, multiB });
+
+      FisherMetrics f = FisherCriteria.CalculateMultiplyCriteria(set);
+      double fExp = 0.75;
+      Assert.AreEqual(fExp, f.F);
+    }
+  }
+}
