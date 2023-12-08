b59ab9e853c05faee5d5c298139f367785405e9b
kradunches <cheryaev@yandex.ru>
2023-12-08 11:01:46 +0300

    added ignorable factor list in FisherTableProcessor, InnerDispersionTest added

--- source/Schicksal/Anova/FisherTableProcessor.cs
+++ source/Schicksal/Anova/FisherTableProcessor.cs
@@ -1,8 +1,7 @@
-﻿using System.Collections.Generic;
-using System.Data;
-using System.Threading.Tasks;
-using Notung;
+﻿using Notung;
 using Schicksal.Basic;
+using System.Collections.Generic;
+using System.Data;
 
 namespace Schicksal.Anova
 {
@@ -34,11 +33,11 @@ namespace Schicksal.Anova
 
       var result = new List<FisherTestResult>(group_count);
 
-      if (this.RunInParrallel)
-      {
-        Parallel.For(1, group_count, (i) => this.ProcessFactor(group_count, result, i));
-      }
-      else
+      //if (this.RunInParrallel)
+      //{
+      //  Parallel.For(1, group_count, (i) => this.ProcessFactor(group_count, result, i));
+      //}
+      //else
       {
         for (int i = 1; i < group_count; i++)
           this.ProcessFactor(group_count, result, i);
@@ -50,16 +49,54 @@ namespace Schicksal.Anova
     private void ProcessFactor(int groupCount, List<FisherTestResult> result, int i)
     {
       var factors = new List<string>();
+      var ignoredFactors = new List<string>();
 
       for (int j = 0; j < m_factors.Length; j++)
       {
         if ((i & (1 << j)) != 0)
           factors.Add(m_factors[j]);
+        else
+          ignoredFactors.Add(m_factors[j]);
       }
 
       lock (result)
         this.ReportProgress(result.Count * 100 / groupCount, string.Join("+", factors));
 
+      if (ignoredFactors.Count == 0)
+      {
+        SingleFactorAnalysis(result, factors);
+      }
+
+      else
+      {
+        using (var groups = new TableSetDataGroup(m_source, factors.ToArray(), ignoredFactors.ToArray(), m_result_column, this.Filter))
+        {
+          FisherMetrics degrees = FisherCriteria.CalculateMultiplyCriteria(groups);
+          if (degrees.Ndf != 0)
+          {
+            var row = new FisherTestResult();
+
+            row.F = degrees.F;
+            row.Kdf = degrees.Kdf;
+            row.Ndf = degrees.Ndf;
+            row.Factor = string.Join("+", factors);
+            row.F005 = FisherCriteria.GetCriticalValue(0.05, degrees.Kdf, degrees.Ndf);
+            row.F001 = FisherCriteria.GetCriticalValue(0.01, degrees.Kdf, degrees.Ndf);
+            row.P = FisherCriteria.GetProbability(degrees);
+
+            lock (result)
+              result.Add(row);
+          }
+          else
+          {
+            SingleFactorAnalysis(result, factors);
+          }
+        }
+      }
+    }
+
+    private void SingleFactorAnalysis(List<FisherTestResult> result, List<string> factors)
+    {
       using (var group = new TableMultyDataGroup(m_source, factors.ToArray(), m_result_column, this.Filter))
       {
         FisherMetrics degrees = FisherCriteria.CalculateCriteria(group);
---
+++ test/Schicksal/ANOVATest/InnerDispersionTest.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Schicksal.Anova;
+using Schicksal.Basic;
+
+namespace ANOVATest
+{
+  [TestClass]
+  public class InnerDispersionTest
+  {
+    //В наборе данных есть 3 фактора (A-2уровня, B-3уровня, C-2уровня), необходимо выполнить многофакторный дисперсионный анализ для одной, двух и трех факторов и проверить, одинакова ли внутренняя дисперсия в результатах анализа с одним, двумя и тремя факторами.
+    [TestMethod]
+    public void NdfTestInThreeGroups()
+    {
+      //Три фактора
+      //A1
+      ArrayDataGroup groupA1B1C1 = new ArrayDataGroup(new double[] { 2, 5, 4 });
+      ArrayDataGroup groupA1B1C2 = new ArrayDataGroup(new double[] { 3, 6, 1 });
+      ArrayDataGroup groupA1B2C1 = new ArrayDataGroup(new double[] { 2, 2, 3 });
+      ArrayDataGroup groupA1B2C2 = new ArrayDataGroup(new double[] { 4, 7, 2 });
+      ArrayDataGroup groupA1B3C1 = new ArrayDataGroup(new double[] { 2, 5, 6 });
+      ArrayDataGroup groupA1B3C2 = new ArrayDataGroup(new double[] { 4, 8, 3 });
+      MultiArrayDataGroup threeMultiA1 = new MultiArrayDataGroup(new IDataGroup[] { groupA1B1C1, groupA1B1C2, groupA1B2C1, groupA1B2C2, groupA1B3C1, groupA1B3C2 });
+
+      //A2
+      ArrayDataGroup groupA2B1C1 = new ArrayDataGroup(new double[] { 3, 5, 5 });
+      ArrayDataGroup groupA2B1C2 = new ArrayDataGroup(new double[] { 6, 1, 2 });
+      ArrayDataGroup groupA2B2C1 = new ArrayDataGroup(new double[] { 2, 3, 5 });
+      ArrayDataGroup groupA2B2C2 = new ArrayDataGroup(new double[] { 4, 7, 5 });
+      ArrayDataGroup groupA2B3C1 = new ArrayDataGroup(new double[] { 8, 1, 2 });
+      ArrayDataGroup groupA2B3C2 = new ArrayDataGroup(new double[] { 4, 3, 4 });
+      MultiArrayDataGroup threeMultiA2 = new MultiArrayDataGroup(new IDataGroup[] { groupA2B1C1, groupA2B1C2, groupA2B2C1, groupA2B2C2, groupA2B3C1, groupA2B3C2 });
+
+      SetMultiArrayDataGroup threeSet = new SetMultiArrayDataGroup(new IMultyDataGroup[] { threeMultiA1, threeMultiA2 });
+      FisherMetrics f3 = FisherCriteria.CalculateMultiplyCriteria(threeSet);
+
+      MultiArrayDataGroup twoMultiA1B1 = new MultiArrayDataGroup(new IDataGroup[] { groupA1B1C1, groupA1B1C2 });
+      MultiArrayDataGroup twoMultiA1B2 = new MultiArrayDataGroup(new IDataGroup[] { groupA1B2C1, groupA1B2C2 });
+      MultiArrayDataGroup twoMultiA1B3 = new MultiArrayDataGroup(new IDataGroup[] { groupA1B3C1, groupA1B3C2 });
+
+      MultiArrayDataGroup twoMultiA2B1 = new MultiArrayDataGroup(new IDataGroup[] { groupA2B1C1, groupA2B1C2 });
+      MultiArrayDataGroup twoMultiA2B2 = new MultiArrayDataGroup(new IDataGroup[] { groupA2B2C1, groupA2B2C2 });
+      MultiArrayDataGroup twoMultiA2B3 = new MultiArrayDataGroup(new IDataGroup[] { groupA2B3C1, groupA2B3C2 });
+
+      SetMultiArrayDataGroup twoSet = new SetMultiArrayDataGroup(new IMultyDataGroup[] { twoMultiA1B1, twoMultiA1B2, twoMultiA1B3, twoMultiA2B1, twoMultiA2B2, twoMultiA2B3 });
+      FisherMetrics f2 = FisherCriteria.CalculateMultiplyCriteria(twoSet);
+
+      MultiArrayDataGroup oneMultiA1B1C1 = new MultiArrayDataGroup(new IDataGroup[] { groupA1B1C1 });
+      MultiArrayDataGroup oneMultiA1B1C2 = new MultiArrayDataGroup(new IDataGroup[] { groupA1B1C2 });
+      MultiArrayDataGroup oneMultiA1B2C1 = new MultiArrayDataGroup(new IDataGroup[] { groupA1B2C1 });
+      MultiArrayDataGroup oneMultiA1B2C2 = new MultiArrayDataGroup(new IDataGroup[] { groupA1B2C2 });
+      MultiArrayDataGroup oneMultiA1B3C1 = new MultiArrayDataGroup(new IDataGroup[] { groupA1B3C1 });
+      MultiArrayDataGroup oneMultiA1B3C2 = new MultiArrayDataGroup(new IDataGroup[] { groupA1B3C2 });
+
+      MultiArrayDataGroup oneMultiA2B1C1 = new MultiArrayDataGroup(new IDataGroup[] { groupA2B1C1 });
+      MultiArrayDataGroup oneMultiA2B1C2 = new MultiArrayDataGroup(new IDataGroup[] { groupA2B1C2 });
+      MultiArrayDataGroup oneMultiA2B2C1 = new MultiArrayDataGroup(new IDataGroup[] { groupA2B2C1 });
+      MultiArrayDataGroup oneMultiA2B2C2 = new MultiArrayDataGroup(new IDataGroup[] { groupA2B2C2 });
+      MultiArrayDataGroup oneMultiA2B3C1 = new MultiArrayDataGroup(new IDataGroup[] { groupA2B3C1 });
+      MultiArrayDataGroup oneMultiA2B3C2 = new MultiArrayDataGroup(new IDataGroup[] { groupA2B3C2 });
+
+      SetMultiArrayDataGroup oneSet = new SetMultiArrayDataGroup(new IMultyDataGroup[] { oneMultiA1B1C1, oneMultiA1B1C2, oneMultiA1B2C1, oneMultiA1B2C2, oneMultiA1B3C1, oneMultiA1B3C2, oneMultiA2B1C1, oneMultiA2B1C2, oneMultiA2B2C1, oneMultiA2B2C2, oneMultiA2B3C1, oneMultiA2B3C2 });
+      FisherMetrics f1 = FisherCriteria.CalculateMultiplyCriteria(oneSet);
+
+      Assert.AreEqual(f3.Ndf, f2.Ndf);
+      Assert.AreEqual(f3.MSw, f2.MSw);
+
+      Assert.AreEqual(f2.Ndf, f1.Ndf);
+      Assert.AreEqual(f2.MSw, f1.MSw);
+    }
+  }
+}
