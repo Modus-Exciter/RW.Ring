007153f2757120c4b2d7bb7969aaa7a92dbb561d
Suparwar <Pashasol228@yandex.ru>
2023-12-24 17:53:20 +0300

    Фильтрация. ИСТ-121 Соловьев Павел
    Сделал фильтры для таблицы.

--- source/Schicksal.Helm/TableForm.Designer.cs
+++ source/Schicksal.Helm/TableForm.Designer.cs
@@ -28,67 +28,94 @@
     /// </summary>
     private void InitializeComponent()
     {
-      this.components = new System.ComponentModel.Container();
-      System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TableForm));
-      this.m_grid = new System.Windows.Forms.DataGridView();
-      this.m_context_menu = new System.Windows.Forms.ContextMenuStrip(this.components);
-      this.m_cmd_tbedit = new System.Windows.Forms.ToolStripMenuItem();
-      this.m_switcher = new Notung.ComponentModel.LanguageSwitcher(this.components);
-      ((System.ComponentModel.ISupportInitialize)(this.m_grid)).BeginInit();
-      this.m_context_menu.SuspendLayout();
-      ((System.ComponentModel.ISupportInitialize)(this.m_switcher)).BeginInit();
-      this.SuspendLayout();
-      // 
-      // m_grid
-      // 
-      this.m_grid.AllowUserToOrderColumns = true;
-      this.m_grid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-      this.m_grid.ContextMenuStrip = this.m_context_menu;
-      this.m_grid.Dock = System.Windows.Forms.DockStyle.Fill;
-      this.m_grid.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
-      this.m_grid.Location = new System.Drawing.Point(0, 0);
-      this.m_grid.Name = "m_grid";
-      this.m_grid.RowHeadersWidth = 62;
-      this.m_grid.Size = new System.Drawing.Size(596, 464);
-      this.m_grid.TabIndex = 0;
-      this.m_grid.CellEndEdit += new System.Windows.Forms.DataGridViewCellEventHandler(this.m_grid_CellEndEdit);
-      this.m_grid.CellEnter += new System.Windows.Forms.DataGridViewCellEventHandler(this.m_grid_CellEnter);
-      this.m_grid.DataError += new System.Windows.Forms.DataGridViewDataErrorEventHandler(this.Grid_DataError);
-      this.m_grid.UserDeletedRow += new System.Windows.Forms.DataGridViewRowEventHandler(this.m_grid_UserDeletedRow);
-      this.m_grid.MouseClick += new System.Windows.Forms.MouseEventHandler(this.m_grid_MouseClick);
-      // 
-      // m_context_menu
-      // 
-      this.m_context_menu.ImageScalingSize = new System.Drawing.Size(24, 24);
-      this.m_context_menu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TableForm));
+            this.m_grid = new System.Windows.Forms.DataGridView();
+            this.m_context_menu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.m_cmd_tbedit = new System.Windows.Forms.ToolStripMenuItem();
+            this.m_switcher = new Notung.ComponentModel.LanguageSwitcher(this.components);
+            this.reset = new System.Windows.Forms.Button();
+            this.DoFilter = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.m_grid)).BeginInit();
+            this.m_context_menu.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.m_switcher)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // m_grid
+            // 
+            this.m_grid.AllowUserToOrderColumns = true;
+            this.m_grid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.m_grid.ContextMenuStrip = this.m_context_menu;
+            this.m_grid.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.m_grid.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.m_grid.Location = new System.Drawing.Point(0, 0);
+            this.m_grid.Margin = new System.Windows.Forms.Padding(4);
+            this.m_grid.Name = "m_grid";
+            this.m_grid.RowHeadersWidth = 62;
+            this.m_grid.Size = new System.Drawing.Size(795, 571);
+            this.m_grid.TabIndex = 0;
+            this.m_grid.CellEndEdit += new System.Windows.Forms.DataGridViewCellEventHandler(this.m_grid_CellEndEdit);
+            this.m_grid.CellEnter += new System.Windows.Forms.DataGridViewCellEventHandler(this.m_grid_CellEnter);
+            this.m_grid.ColumnHeaderMouseDoubleClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.m_grid_ColumnHeaderMouseDoubleClick);
+            this.m_grid.DataError += new System.Windows.Forms.DataGridViewDataErrorEventHandler(this.Grid_DataError);
+            this.m_grid.UserDeletedRow += new System.Windows.Forms.DataGridViewRowEventHandler(this.m_grid_UserDeletedRow);
+            this.m_grid.MouseClick += new System.Windows.Forms.MouseEventHandler(this.m_grid_MouseClick);
+            // 
+            // m_context_menu
+            // 
+            this.m_context_menu.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.m_context_menu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.m_cmd_tbedit});
-      this.m_context_menu.Name = "m_context_menu";
-      this.m_context_menu.Size = new System.Drawing.Size(181, 48);
-      this.m_context_menu.Opening += new System.ComponentModel.CancelEventHandler(this.m_context_menu_Opening);
-      // 
-      // m_cmd_tbedit
-      // 
-      this.m_cmd_tbedit.Name = "m_cmd_tbedit";
-      this.m_cmd_tbedit.Size = new System.Drawing.Size(180, 22);
-      this.m_cmd_tbedit.Text = "Edit table";
-      this.m_cmd_tbedit.Click += new System.EventHandler(this.m_cmd_tbedit_Click);
-      // 
-      // m_switcher
-      // 
-      this.m_switcher.LanguageChanged += new System.EventHandler<Notung.ComponentModel.LanguageEventArgs>(this.Switcher_LanguageChanged);
-      // 
-      // TableForm
-      // 
-      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-      this.ClientSize = new System.Drawing.Size(596, 464);
-      this.Controls.Add(this.m_grid);
-      this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-      this.Name = "TableForm";
-      ((System.ComponentModel.ISupportInitialize)(this.m_grid)).EndInit();
-      this.m_context_menu.ResumeLayout(false);
-      ((System.ComponentModel.ISupportInitialize)(this.m_switcher)).EndInit();
-      this.ResumeLayout(false);
+            this.m_context_menu.Name = "m_context_menu";
+            this.m_context_menu.Size = new System.Drawing.Size(143, 28);
+            this.m_context_menu.Opening += new System.ComponentModel.CancelEventHandler(this.m_context_menu_Opening);
+            // 
+            // m_cmd_tbedit
+            // 
+            this.m_cmd_tbedit.Name = "m_cmd_tbedit";
+            this.m_cmd_tbedit.Size = new System.Drawing.Size(142, 24);
+            this.m_cmd_tbedit.Text = "Edit table";
+            this.m_cmd_tbedit.Click += new System.EventHandler(this.m_cmd_tbedit_Click);
+            // 
+            // m_switcher
+            // 
+            this.m_switcher.LanguageChanged += new System.EventHandler<Notung.ComponentModel.LanguageEventArgs>(this.Switcher_LanguageChanged);
+            // 
+            // reset
+            // 
+            this.reset.Location = new System.Drawing.Point(24, 0);
+            this.reset.Name = "reset";
+            this.reset.Size = new System.Drawing.Size(25, 23);
+            this.reset.TabIndex = 2;
+            this.reset.Text = "X";
+            this.reset.UseVisualStyleBackColor = true;
+            this.reset.Click += new System.EventHandler(this.reset_Click);
+            // 
+            // DoFilter
+            // 
+            this.DoFilter.Location = new System.Drawing.Point(0, 0);
+            this.DoFilter.Name = "DoFilter";
+            this.DoFilter.Size = new System.Drawing.Size(27, 23);
+            this.DoFilter.TabIndex = 3;
+            this.DoFilter.Text = "🔍︎";
+            this.DoFilter.UseVisualStyleBackColor = true;
+            this.DoFilter.Click += new System.EventHandler(this.DoFilter_Click);
+            // 
+            // TableForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(795, 571);
+            this.Controls.Add(this.DoFilter);
+            this.Controls.Add(this.reset);
+            this.Controls.Add(this.m_grid);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "TableForm";
+            ((System.ComponentModel.ISupportInitialize)(this.m_grid)).EndInit();
+            this.m_context_menu.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.m_switcher)).EndInit();
+            this.ResumeLayout(false);
 
     }
 
@@ -98,5 +125,7 @@
     private System.Windows.Forms.ContextMenuStrip m_context_menu;
     private Notung.ComponentModel.LanguageSwitcher m_switcher;
         private System.Windows.Forms.ToolStripMenuItem m_cmd_tbedit;
+        private System.Windows.Forms.Button reset;
+        private System.Windows.Forms.Button DoFilter;
     }
 }
--- source/Schicksal.Helm/TableForm.cs
+++ source/Schicksal.Helm/TableForm.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.IO;
+using System.Linq;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 using Notung;
 using Notung.Services;
 using Schicksal.Helm.Dialogs;
@@ -11,199 +14,281 @@ using Schicksal.Helm.Properties;
 
 namespace Schicksal.Helm
 {
-  public partial class TableForm : Form
-  {
-    public TableForm()
+    public partial class TableForm : Form
     {
-      this.InitializeComponent();
-    }
+        TextBox[] customHeaderPanel;
 
-    public DataTable DataSource
-    {
-      get { return m_grid.DataSource as DataTable; }
-      set
-      {
-        if (ReferenceEquals(value, m_grid.DataSource))
-          return;
-
-        m_grid.DataSource = value;
-
-        if (value != null)
+        public TableForm()
         {
-          value.AcceptChanges();
+            this.Padding = new Padding(0, 25, 0, 0);
+            this.InitializeComponent();
+        }
 
-          if (m_grid.ReadOnly)
-          {
-            foreach (DataGridViewColumn col in m_grid.Columns)
+        public DataTable DataSource
+        {
+            get { return m_grid.DataSource as DataTable; }
+            set
             {
-              if (col.ValueType == typeof(double) || col.ValueType == typeof(float))
-                col.DefaultCellStyle.Format = "0.000";
+                if (ReferenceEquals(value, m_grid.DataSource))
+                    return;
+
+                m_grid.DataSource = value;
+
+                // инициализация массива полей
+                customHeaderPanel = new TextBox[m_grid.ColumnCount];
+
+                if (value != null)
+                {
+                    value.AcceptChanges();
+
+                    if (m_grid.ReadOnly)
+                    {
+                        foreach (DataGridViewColumn col in m_grid.Columns)
+                        {
+                            if (col.ValueType == typeof(double) || col.ValueType == typeof(float))
+                                col.DefaultCellStyle.Format = "0.000";
+                        }
+                    }
+                }
             }
-          }
         }
-      }
-    }
 
-    public string FileName { get; set; }
+        public string FileName { get; set; }
 
-    public void MarkAsReadOnly()
-    {
-      m_grid.ReadOnly = true;
-      m_grid.AllowUserToAddRows = false;
-      m_grid.AllowUserToDeleteRows = false;
-    }
-
-    protected override void OnLoad(EventArgs e)
-    {
-      base.OnLoad(e);
-
-      this.Icon = this.Icon.Clone() as Icon;
-    }
-
-    protected override void OnShown(System.EventArgs e)
-    {
-      base.OnShown(e);
-
-      if (m_grid.Rows.Count < (1 << 10))
-        m_grid.AutoResizeColumns();
-    }
-
-    protected override void OnClosing(CancelEventArgs e)
-    {
-      base.OnClosing(e);
-
-      if (this.DataSource != null && this.DataSource.GetChanges() != null)
-      {
-        var res = AppManager.Notificator.ConfirmOrCancel(string.Format(
-          Resources.SAVE_FILE_CONFIRM, this.Text.Substring(0, this.Text.Length - 1)), InfoLevel.Info);
-
-        if (res == null)
-          e.Cancel = true;
-        else if (res == true)
+        public void MarkAsReadOnly()
         {
-          this.Save();
-
-          var parent = this.MdiParent as MainForm;
-
-          if (parent != null)
-            parent.FillLastFilesMenu();
+            m_grid.ReadOnly = true;
+            m_grid.AllowUserToAddRows = false;
+            m_grid.AllowUserToDeleteRows = false;
         }
-      }
-    }
 
-    private void SaveFile(string fileName, DataTable table)
-    {
-      if (File.Exists(fileName))
-        File.Delete(fileName);
-
-      table.AcceptChanges();
-
-      using (var fs = new FileStream(fileName, FileMode.Create, FileAccess.Write))
-      {
-        DataTableSaver.WriteDataTable(table, fs);
-      }
-
-      AppManager.Configurator.GetSection<Program.Preferences>().LastFiles[fileName] = DateTime.Now;
-
-      this.FileName = Path.GetFileName(this.FileName);
-      this.Text = Path.GetFileName(this.FileName);
-    }
-
-    public void Save()
-    {
-      if (string.IsNullOrEmpty(this.FileName))
-        this.SaveAs();
-      else
-        this.SaveFile(this.FileName, this.DataSource);
-    }
-
-    public void SaveAs()
-    {
-      using (var dlg = new SaveFileDialog())
-      {
-        dlg.Filter = "Schicksal data files|*.sks";
-
-        if (dlg.ShowDialog(this) == System.Windows.Forms.DialogResult.OK)
+        protected override void OnLoad(EventArgs e)
         {
-          this.FileName = dlg.FileName;
-          this.SaveFile(this.FileName, this.DataSource);
+            base.OnLoad(e);
+
+            this.Icon = this.Icon.Clone() as Icon;
         }
-      }
-    }
 
-    private void Grid_DataError(object sender, DataGridViewDataErrorEventArgs e)
-    {
-      AppManager.Notificator.Show(e.Exception.Message, InfoLevel.Error);
-    }
-
-    private void Switcher_LanguageChanged(object sender, Notung.ComponentModel.LanguageEventArgs e)
-    {
-      m_cmd_tbedit.Text = Resources.TABLE_EDIT;
-    }
-
-    private void m_cmd_tbedit_Click(object sender, EventArgs e)
-    {
-      using (var dlg = new EditColumnsDialog())
-      {
-        dlg.Text = Resources.TABLE_EDIT;
-        TableColumnInfo.FillColumnInfo(dlg.Columns, this.DataSource);
-
-        if (dlg.ShowDialog(this) == System.Windows.Forms.DialogResult.OK)
+        protected override void OnShown(System.EventArgs e)
         {
-          m_grid.DataSource = TableColumnInfo.CreateUpdatedTable(dlg.Columns, this.DataSource);
+            base.OnShown(e);
 
-          if (!this.Text.EndsWith("*"))
-            this.Text += "*";
+            if (m_grid.Rows.Count < (1 << 10))
+                m_grid.AutoResizeColumns();
         }
-      }
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            base.OnClosing(e);
+
+            if (this.DataSource != null && this.DataSource.GetChanges() != null)
+            {
+                var res = AppManager.Notificator.ConfirmOrCancel(string.Format(
+                  Resources.SAVE_FILE_CONFIRM, this.Text.Substring(0, this.Text.Length - 1)), InfoLevel.Info);
+
+                if (res == null)
+                    e.Cancel = true;
+                else if (res == true)
+                {
+                    this.Save();
+
+                    var parent = this.MdiParent as MainForm;
+
+                    if (parent != null)
+                        parent.FillLastFilesMenu();
+                }
+            }
+        }
+
+        private void SaveFile(string fileName, DataTable table)
+        {
+            if (File.Exists(fileName))
+                File.Delete(fileName);
+
+            table.AcceptChanges();
+
+            using (var fs = new FileStream(fileName, FileMode.Create, FileAccess.Write))
+            {
+                DataTableSaver.WriteDataTable(table, fs);
+            }
+
+            AppManager.Configurator.GetSection<Program.Preferences>().LastFiles[fileName] = DateTime.Now;
+
+            this.FileName = Path.GetFileName(this.FileName);
+            this.Text = Path.GetFileName(this.FileName);
+        }
+
+        public void Save()
+        {
+            if (string.IsNullOrEmpty(this.FileName))
+                this.SaveAs();
+            else
+                this.SaveFile(this.FileName, this.DataSource);
+        }
+
+        public void SaveAs()
+        {
+            using (var dlg = new SaveFileDialog())
+            {
+                dlg.Filter = "Schicksal data files|*.sks";
+
+                if (dlg.ShowDialog(this) == System.Windows.Forms.DialogResult.OK)
+                {
+                    this.FileName = dlg.FileName;
+                    this.SaveFile(this.FileName, this.DataSource);
+                }
+            }
+        }
+
+        private void Grid_DataError(object sender, DataGridViewDataErrorEventArgs e)
+        {
+            AppManager.Notificator.Show(e.Exception.Message, InfoLevel.Error);
+        }
+
+        private void Switcher_LanguageChanged(object sender, Notung.ComponentModel.LanguageEventArgs e)
+        {
+            m_cmd_tbedit.Text = Resources.TABLE_EDIT;
+        }
+
+        private void m_cmd_tbedit_Click(object sender, EventArgs e)
+        {
+            using (var dlg = new EditColumnsDialog())
+            {
+                dlg.Text = Resources.TABLE_EDIT;
+                TableColumnInfo.FillColumnInfo(dlg.Columns, this.DataSource);
+
+                if (dlg.ShowDialog(this) == System.Windows.Forms.DialogResult.OK)
+                {
+                    m_grid.DataSource = TableColumnInfo.CreateUpdatedTable(dlg.Columns, this.DataSource);
+
+                    if (!this.Text.EndsWith("*"))
+                        this.Text += "*";
+                }
+            }
+        }
+
+        private void m_grid_CellEnter(object sender, DataGridViewCellEventArgs e)
+        {
+            m_grid.BeginEdit(true);
+        }
+
+        private void m_grid_MouseClick(object sender, MouseEventArgs e)
+        {
+            var info = m_grid.HitTest(e.X, e.Y);
+            DataGridViewCell cell = null;
+
+            if (info != null && info.ColumnIndex >= 0 && info.RowIndex >= 0)
+                cell = m_grid.Rows[info.RowIndex].Cells[info.ColumnIndex];
+
+            if (m_grid.CurrentCell != null && m_grid.CurrentCell != cell)
+            {
+                if (m_grid.CurrentCell.IsInEditMode)
+                    m_grid.EndEdit();
+                else
+                    m_grid.BeginEdit(false);
+            }
+        }
+
+        private void m_grid_UserDeletedRow(object sender, DataGridViewRowEventArgs e)
+        {
+            if (!this.Text.EndsWith("*"))
+                this.Text += "*";
+        }
+
+        private void m_grid_CellEndEdit(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex < 0)
+                return;
+
+            var row = m_grid.Rows[e.RowIndex].DataBoundItem as DataRowView;
+
+            if (row == null)
+                return;
+
+            if (row.Row.RowState != DataRowState.Unchanged && !this.Text.EndsWith("*"))
+                this.Text += "*";
+        }
+
+        private void m_context_menu_Opening(object sender, CancelEventArgs e)
+        {
+            if (m_grid.ReadOnly)
+                e.Cancel = true;
+        }
+
+
+
+
+
+
+
+
+        private List<string> GetColumnNames()
+        {
+            List<string> names = new List<string>();
+
+            DataTable dataTable = m_grid.DataSource as DataTable;
+            if (dataTable != null)
+            {
+                foreach (DataColumn column in dataTable.Columns)
+                {
+                    names.Add(column.ColumnName);
+                }
+            }
+
+            return names;
+        }
+
+        private void reset_Click(object sender, EventArgs e)
+        {
+            // очистка панли управления и массива полей
+            foreach (var textBox in customHeaderPanel)
+            {
+                m_grid.Parent.Controls.Remove(textBox);
+            }
+            Array.Clear(customHeaderPanel, 0, customHeaderPanel.Length);
+            (m_grid.DataSource as DataTable).DefaultView.RowFilter = "";
+        }
+
+        private void m_grid_ColumnHeaderMouseDoubleClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            // Проверка, является ли щелчок мыши на заголовке столбца
+            if (e.Button == MouseButtons.Left && e.RowIndex == -1)
+            {
+                int index = e.ColumnIndex;
+                if (customHeaderPanel[index] == null)
+                {
+                    TextBox textBox = new TextBox();
+
+                    // Добавляем текстовое поле в массив
+                    customHeaderPanel[index] = textBox;
+
+                    // Позиционирование поля относительно щелкнутого заголовка столбца
+                    Rectangle headerRect = m_grid.GetCellDisplayRectangle(e.ColumnIndex, -1, true);
+                    textBox.Location = new Point(headerRect.X, headerRect.Y);
+                    textBox.Size = new Size(headerRect.Width, headerRect.Height);
+
+                    // Добавление поля к родительскому контейнеру DataGridView
+                    m_grid.Parent.Controls.Add(textBox);
+                }
+            }
+        }
+
+        private void DoFilter_Click(object sender, EventArgs e)
+        {
+            string filterExpression = "";
+            List<string> columnNames = GetColumnNames();
+
+            for (int i = 0; i < customHeaderPanel.Length; i++)
+            {
+                if (customHeaderPanel[i] != null)
+                {
+                    filterExpression += $"Convert([{columnNames[i]}], 'System.String') LIKE '%{customHeaderPanel[i].Text}%' AND ";
+                }
+            }
+
+            filterExpression = filterExpression.TrimEnd(" AND ".ToCharArray()); // Удалить последний " AND "
+
+            (m_grid.DataSource as DataTable).DefaultView.RowFilter = filterExpression;
+        }
     }
-
-    private void m_grid_CellEnter(object sender, DataGridViewCellEventArgs e)
-    {
-      m_grid.BeginEdit(true);
-    }
-
-    private void m_grid_MouseClick(object sender, MouseEventArgs e)
-    {
-      var info = m_grid.HitTest(e.X, e.Y);
-      DataGridViewCell cell = null;
-
-      if (info != null && info.ColumnIndex >= 0 && info.RowIndex >= 0)
-        cell = m_grid.Rows[info.RowIndex].Cells[info.ColumnIndex];
-
-      if (m_grid.CurrentCell != null && m_grid.CurrentCell != cell)
-      {
-        if (m_grid.CurrentCell.IsInEditMode)
-          m_grid.EndEdit();
-        else
-          m_grid.BeginEdit(false);
-      }
-    }
-
-    private void m_grid_UserDeletedRow(object sender, DataGridViewRowEventArgs e)
-    {
-      if(!this.Text.EndsWith("*"))
-        this.Text += "*";
-    }
-
-    private void m_grid_CellEndEdit(object sender, DataGridViewCellEventArgs e)
-    {
-      if (e.RowIndex < 0)
-        return;
-
-      var row = m_grid.Rows[e.RowIndex].DataBoundItem as DataRowView;
-
-      if (row == null)
-        return;
-
-      if (row.Row.RowState != DataRowState.Unchanged && !this.Text.EndsWith("*"))
-        this.Text += "*";
-    }
-
-    private void m_context_menu_Opening(object sender, CancelEventArgs e)
-    {
-      if (m_grid.ReadOnly)
-        e.Cancel = true;
-    }
-  }
 }
