71a8f460ae1e6f1ff332d8d282bec1ea0c945a13
Сергей Медведев <glorguint@mail.ru>
2023-12-28 18:18:06 +0300

    ClusteringParametersDialog

---
+++ source/Schicksal.Helm/Dialogs/ClusteringParametersDialog.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Schicksal.Helm.Dialogs
+{
+  public partial class ClusteringParametersDialog : Form
+  {
+    public ClusteringParametersDialog()
+    {
+      InitializeComponent();
+    }
+  }
+}
