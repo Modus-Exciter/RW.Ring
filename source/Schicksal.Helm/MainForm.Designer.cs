df599deb258058b474facdc5165c8853f8d70e9b
Сергей Медведев <glorguint@mail.ru>
2024-02-21 18:00:43 +0300

    НСР

--- source/Schicksal.Helm/MainForm.Designer.cs
+++ source/Schicksal.Helm/MainForm.Designer.cs
@@ -76,7 +76,8 @@
       this.m_main_menu.Location = new System.Drawing.Point(0, 0);
       this.m_main_menu.MdiWindowListItem = this.m_menu_windows;
       this.m_main_menu.Name = "m_main_menu";
-      this.m_main_menu.Size = new System.Drawing.Size(912, 28);
+      this.m_main_menu.Padding = new System.Windows.Forms.Padding(4, 2, 0, 2);
+      this.m_main_menu.Size = new System.Drawing.Size(684, 24);
       this.m_main_menu.TabIndex = 1;
       this.m_main_menu.Text = "menuStrip1";
       // 
@@ -93,7 +94,7 @@
             this.m_cmd_settings});
       this.m_menu_file.MergeIndex = 0;
       this.m_menu_file.Name = "m_menu_file";
-      this.m_menu_file.Size = new System.Drawing.Size(44, 24);
+      this.m_menu_file.Size = new System.Drawing.Size(37, 20);
       this.m_menu_file.Text = "File";
       // 
       // m_cmd_new
@@ -101,7 +102,7 @@
       this.m_cmd_new.Image = global::Schicksal.Helm.Properties.Resources.document_new;
       this.m_cmd_new.Name = "m_cmd_new";
       this.m_cmd_new.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.N)));
-      this.m_cmd_new.Size = new System.Drawing.Size(167, 24);
+      this.m_cmd_new.Size = new System.Drawing.Size(146, 22);
       this.m_cmd_new.Text = "New";
       this.m_cmd_new.Click += new System.EventHandler(this.Cmd_new_Click);
       // 
@@ -110,7 +111,7 @@
       this.m_cmd_open.Image = global::Schicksal.Helm.Properties.Resources.folder;
       this.m_cmd_open.Name = "m_cmd_open";
       this.m_cmd_open.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
-      this.m_cmd_open.Size = new System.Drawing.Size(167, 24);
+      this.m_cmd_open.Size = new System.Drawing.Size(146, 22);
       this.m_cmd_open.Text = "Open";
       this.m_cmd_open.Click += new System.EventHandler(this.Cmd_open_Click);
       // 
@@ -120,7 +121,7 @@
       this.m_cmd_save.Image = global::Schicksal.Helm.Properties.Resources.disk_blue;
       this.m_cmd_save.Name = "m_cmd_save";
       this.m_cmd_save.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
-      this.m_cmd_save.Size = new System.Drawing.Size(167, 24);
+      this.m_cmd_save.Size = new System.Drawing.Size(146, 22);
       this.m_cmd_save.Text = "Save";
       this.m_cmd_save.Click += new System.EventHandler(this.Cmd_save_Click);
       // 
@@ -129,39 +130,39 @@
       this.m_cmd_save_as.Enabled = false;
       this.m_cmd_save_as.Image = global::Schicksal.Helm.Properties.Resources.save_as_icon;
       this.m_cmd_save_as.Name = "m_cmd_save_as";
-      this.m_cmd_save_as.Size = new System.Drawing.Size(167, 24);
+      this.m_cmd_save_as.Size = new System.Drawing.Size(146, 22);
       this.m_cmd_save_as.Text = "Save As";
       this.m_cmd_save_as.Click += new System.EventHandler(this.Cmd_save_as_Click);
       // 
       // m_last_files_separator
       // 
       this.m_last_files_separator.Name = "m_last_files_separator";
-      this.m_last_files_separator.Size = new System.Drawing.Size(164, 6);
+      this.m_last_files_separator.Size = new System.Drawing.Size(143, 6);
       // 
       // m_menu_last_files
       // 
       this.m_menu_last_files.Name = "m_menu_last_files";
-      this.m_menu_last_files.Size = new System.Drawing.Size(167, 24);
+      this.m_menu_last_files.Size = new System.Drawing.Size(146, 22);
       this.m_menu_last_files.Text = "Last files";
       this.m_menu_last_files.DropDownItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.Menu_last_files_DropDownItemClicked);
       // 
       // toolStripSeparator2
       // 
       this.toolStripSeparator2.Name = "toolStripSeparator2";
-      this.toolStripSeparator2.Size = new System.Drawing.Size(164, 6);
+      this.toolStripSeparator2.Size = new System.Drawing.Size(143, 6);
       // 
       // m_cmd_settings
       // 
       this.m_cmd_settings.Image = global::Schicksal.Helm.Properties.Resources.gear_preferences;
       this.m_cmd_settings.Name = "m_cmd_settings";
-      this.m_cmd_settings.Size = new System.Drawing.Size(167, 24);
+      this.m_cmd_settings.Size = new System.Drawing.Size(146, 22);
       this.m_cmd_settings.Text = "Settings";
       this.m_cmd_settings.Click += new System.EventHandler(this.SettingsToolStripMenuItem_Click);
       // 
       // m_menu_import
       // 
       this.m_menu_import.Name = "m_menu_import";
-      this.m_menu_import.Size = new System.Drawing.Size(66, 24);
+      this.m_menu_import.Size = new System.Drawing.Size(55, 20);
       this.m_menu_import.Text = "Import";
       this.m_menu_import.DropDownItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.Menu_import_DropDownItemClicked);
       // 
@@ -174,7 +175,7 @@
             this.m_cmd_clustering});
       this.m_menu_analyze.Enabled = false;
       this.m_menu_analyze.Name = "m_menu_analyze";
-      this.m_menu_analyze.Size = new System.Drawing.Size(73, 24);
+      this.m_menu_analyze.Size = new System.Drawing.Size(60, 20);
       this.m_menu_analyze.Text = "Analyze";
       this.m_menu_analyze.Click += new System.EventHandler(this.m_menu_analyze_Click);
       // 
@@ -182,7 +183,7 @@
       // 
       this.m_cmd_basic.Image = global::Schicksal.Helm.Properties.Resources.table_sql;
       this.m_cmd_basic.Name = "m_cmd_basic";
-      this.m_cmd_basic.Size = new System.Drawing.Size(176, 26);
+      this.m_cmd_basic.Size = new System.Drawing.Size(149, 22);
       this.m_cmd_basic.Text = "Basic statistics";
       this.m_cmd_basic.Click += new System.EventHandler(this.Cmd_basic_Click);
       // 
@@ -190,7 +191,7 @@
       // 
       this.m_cmd_anova.Image = ((System.Drawing.Image)(resources.GetObject("m_cmd_anova.Image")));
       this.m_cmd_anova.Name = "m_cmd_anova";
-      this.m_cmd_anova.Size = new System.Drawing.Size(176, 26);
+      this.m_cmd_anova.Size = new System.Drawing.Size(149, 22);
       this.m_cmd_anova.Text = "ANOVA";
       this.m_cmd_anova.Click += new System.EventHandler(this.Cmd_anova_Click);
       // 
@@ -198,7 +199,7 @@
       // 
       this.m_cmd_ancova.Image = global::Schicksal.Helm.Properties.Resources.line_chart;
       this.m_cmd_ancova.Name = "m_cmd_ancova";
-      this.m_cmd_ancova.Size = new System.Drawing.Size(176, 26);
+      this.m_cmd_ancova.Size = new System.Drawing.Size(149, 22);
       this.m_cmd_ancova.Text = "ANCOVA";
       this.m_cmd_ancova.Click += new System.EventHandler(this.AncovaToolStripMenuItem_Click);
       // 
@@ -206,7 +207,7 @@
       // 
       this.m_cmd_clustering.Image = global::Schicksal.Helm.Properties.Resources.dot_chart;
       this.m_cmd_clustering.Name = "m_cmd_clustering";
-      this.m_cmd_clustering.Size = new System.Drawing.Size(176, 26);
+      this.m_cmd_clustering.Size = new System.Drawing.Size(149, 22);
       this.m_cmd_clustering.Text = "Clustering";
       this.m_cmd_clustering.Click += new System.EventHandler(this.clusteringToolStripMenuItem_Click);
       // 
@@ -218,42 +219,42 @@
             this.m_cmd_fisher_1,
             this.m_cmd_chi2});
       this.m_menu_standard_tables.Name = "m_menu_standard_tables";
-      this.m_menu_standard_tables.Size = new System.Drawing.Size(122, 24);
+      this.m_menu_standard_tables.Size = new System.Drawing.Size(99, 20);
       this.m_menu_standard_tables.Text = "StandardTables";
       // 
       // m_cmd_student
       // 
       this.m_cmd_student.Name = "m_cmd_student";
-      this.m_cmd_student.Size = new System.Drawing.Size(143, 24);
+      this.m_cmd_student.Size = new System.Drawing.Size(180, 22);
       this.m_cmd_student.Text = "Student";
       this.m_cmd_student.Click += new System.EventHandler(this.StudentToolStripMenuItem_Click);
       // 
       // m_cmd_fisher_5
       // 
       this.m_cmd_fisher_5.Name = "m_cmd_fisher_5";
-      this.m_cmd_fisher_5.Size = new System.Drawing.Size(143, 24);
+      this.m_cmd_fisher_5.Size = new System.Drawing.Size(180, 22);
       this.m_cmd_fisher_5.Text = "Fisher, 5%";
       this.m_cmd_fisher_5.Click += new System.EventHandler(this.Fisher5ToolStripMenuItem_Click);
       // 
       // m_cmd_fisher_1
       // 
       this.m_cmd_fisher_1.Name = "m_cmd_fisher_1";
-      this.m_cmd_fisher_1.Size = new System.Drawing.Size(143, 24);
+      this.m_cmd_fisher_1.Size = new System.Drawing.Size(180, 22);
       this.m_cmd_fisher_1.Text = "Fisher, 1%";
       this.m_cmd_fisher_1.Click += new System.EventHandler(this.Fisher1ToolStripMenuItem_Click);
       // 
       // m_cmd_chi2
       // 
       this.m_cmd_chi2.Name = "m_cmd_chi2";
-      this.m_cmd_chi2.Size = new System.Drawing.Size(143, 24);
-      this.m_cmd_chi2.Text = "X^2";
+      this.m_cmd_chi2.Size = new System.Drawing.Size(180, 22);
+      this.m_cmd_chi2.Text = "χ²";
       this.m_cmd_chi2.Click += new System.EventHandler(this.X2ToolStripMenuItem_Click);
       // 
       // m_menu_windows
       // 
       this.m_menu_windows.MergeIndex = 10;
       this.m_menu_windows.Name = "m_menu_windows";
-      this.m_menu_windows.Size = new System.Drawing.Size(82, 24);
+      this.m_menu_windows.Size = new System.Drawing.Size(68, 20);
       this.m_menu_windows.Text = "Windows";
       // 
       // m_menu_help
@@ -262,27 +263,26 @@
             this.m_cmd_about});
       this.m_menu_help.MergeIndex = 20;
       this.m_menu_help.Name = "m_menu_help";
-      this.m_menu_help.Size = new System.Drawing.Size(53, 24);
+      this.m_menu_help.Size = new System.Drawing.Size(44, 20);
       this.m_menu_help.Text = "Help";
       // 
       // m_cmd_about
       // 
       this.m_cmd_about.Image = global::Schicksal.Helm.Properties.Resources.About_Picture;
       this.m_cmd_about.Name = "m_cmd_about";
-      this.m_cmd_about.Size = new System.Drawing.Size(119, 24);
+      this.m_cmd_about.Size = new System.Drawing.Size(107, 22);
       this.m_cmd_about.Text = "About";
       this.m_cmd_about.Click += new System.EventHandler(this.Cmd_about_Click);
       // 
       // MainForm
       // 
-      this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-      this.ClientSize = new System.Drawing.Size(912, 599);
+      this.ClientSize = new System.Drawing.Size(684, 487);
       this.Controls.Add(this.m_main_menu);
       this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
       this.IsMdiContainer = true;
       this.MainMenuStrip = this.m_main_menu;
-      this.Margin = new System.Windows.Forms.Padding(4);
       this.Name = "MainForm";
       this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
       this.Text = "Shicksal";
