d247a3eb5f219f85dfa428b001f422630db6eec4
kradunches <sasha10131310@gmail.com>
2024-03-06 09:53:05 +0300

    Normalization buttons added for "Options" form

--- source/Schicksal.Helm/Dialogs/AnovaOptionsDialog.Designer.cs
+++ source/Schicksal.Helm/Dialogs/AnovaOptionsDialog.Designer.cs
@@ -29,68 +29,91 @@ namespace Schicksal.Helm.Dialogs
     /// </summary>
     private void InitializeComponent()
     {
-      this.m_bottom_panel = new System.Windows.Forms.FlowLayoutPanel();
-      this.m_button_cancel = new System.Windows.Forms.Button();
-      this.m_button_ok = new System.Windows.Forms.Button();
-      this.m_bottom_panel.SuspendLayout();
-      this.SuspendLayout();
-      // 
-      // m_bottom_panel
-      // 
-      this.m_bottom_panel.AutoSize = true;
-      this.m_bottom_panel.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-      this.m_bottom_panel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-      this.m_bottom_panel.Controls.Add(this.m_button_cancel);
-      this.m_bottom_panel.Controls.Add(this.m_button_ok);
-      this.m_bottom_panel.Dock = System.Windows.Forms.DockStyle.Bottom;
-      this.m_bottom_panel.FlowDirection = System.Windows.Forms.FlowDirection.RightToLeft;
-      this.m_bottom_panel.Location = new System.Drawing.Point(0, 398);
-      this.m_bottom_panel.Margin = new System.Windows.Forms.Padding(4);
-      this.m_bottom_panel.Name = "m_bottom_panel";
-      this.m_bottom_panel.Padding = new System.Windows.Forms.Padding(5);
-      this.m_bottom_panel.Size = new System.Drawing.Size(800, 52);
-      this.m_bottom_panel.TabIndex = 2;
-      // 
-      // m_button_cancel
-      // 
-      this.m_button_cancel.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-      this.m_button_cancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-      this.m_button_cancel.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-      this.m_button_cancel.Location = new System.Drawing.Point(684, 9);
-      this.m_button_cancel.Margin = new System.Windows.Forms.Padding(4);
-      this.m_button_cancel.Name = "m_button_cancel";
-      this.m_button_cancel.Size = new System.Drawing.Size(100, 31);
-      this.m_button_cancel.TabIndex = 0;
-      this.m_button_cancel.Text = "Cancel";
-      this.m_button_cancel.UseVisualStyleBackColor = true;
-      // 
-      // m_button_ok
-      // 
-      this.m_button_ok.AutoSize = true;
-      this.m_button_ok.DialogResult = System.Windows.Forms.DialogResult.OK;
-      this.m_button_ok.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-      this.m_button_ok.Location = new System.Drawing.Point(575, 9);
-      this.m_button_ok.Margin = new System.Windows.Forms.Padding(4);
-      this.m_button_ok.Name = "m_button_ok";
-      this.m_button_ok.Size = new System.Drawing.Size(101, 32);
-      this.m_button_ok.TabIndex = 1;
-      this.m_button_ok.Text = "OK";
-      this.m_button_ok.UseVisualStyleBackColor = true;
-      // 
-      // AnovaOptionsDialog
-      // 
-      this.AcceptButton = this.m_button_ok;
-      this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
-      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-      this.CancelButton = this.m_button_cancel;
-      this.ClientSize = new System.Drawing.Size(800, 450);
-      this.Controls.Add(this.m_bottom_panel);
-      this.Name = "AnovaOptionsDialog";
-      this.Text = "AnovaOptionsDialog";
-      this.m_bottom_panel.ResumeLayout(false);
-      this.m_bottom_panel.PerformLayout();
-      this.ResumeLayout(false);
-      this.PerformLayout();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AnovaOptionsDialog));
+            this.m_bottom_panel = new System.Windows.Forms.FlowLayoutPanel();
+            this.m_button_cancel = new System.Windows.Forms.Button();
+            this.m_button_ok = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.m_bottom_panel.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // m_bottom_panel
+            // 
+            resources.ApplyResources(this.m_bottom_panel, "m_bottom_panel");
+            this.m_bottom_panel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.m_bottom_panel.Controls.Add(this.m_button_cancel);
+            this.m_bottom_panel.Controls.Add(this.m_button_ok);
+            this.m_bottom_panel.Name = "m_bottom_panel";
+            // 
+            // m_button_cancel
+            // 
+            resources.ApplyResources(this.m_button_cancel, "m_button_cancel");
+            this.m_button_cancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.m_button_cancel.Name = "m_button_cancel";
+            this.m_button_cancel.UseVisualStyleBackColor = true;
+            // 
+            // m_button_ok
+            // 
+            resources.ApplyResources(this.m_button_ok, "m_button_ok");
+            this.m_button_ok.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.m_button_ok.Name = "m_button_ok";
+            this.m_button_ok.UseVisualStyleBackColor = true;
+            // 
+            // tableLayoutPanel1
+            // 
+            resources.ApplyResources(this.tableLayoutPanel1, "tableLayoutPanel1");
+            this.tableLayoutPanel1.Controls.Add(this.radioButton1, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.radioButton2, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.radioButton3, 3, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            // 
+            // radioButton1
+            // 
+            resources.ApplyResources(this.radioButton1, "radioButton1");
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.TabStop = true;
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            resources.ApplyResources(this.radioButton2, "radioButton2");
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.TabStop = true;
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            resources.ApplyResources(this.radioButton3, "radioButton3");
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.TabStop = true;
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // AnovaOptionsDialog
+            // 
+            this.AcceptButton = this.m_button_ok;
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.m_button_cancel;
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.m_bottom_panel);
+            this.Name = "AnovaOptionsDialog";
+            this.m_bottom_panel.ResumeLayout(false);
+            this.m_bottom_panel.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
     }
 
@@ -99,5 +122,10 @@ namespace Schicksal.Helm.Dialogs
     private System.Windows.Forms.FlowLayoutPanel m_bottom_panel;
     private System.Windows.Forms.Button m_button_cancel;
     private System.Windows.Forms.Button m_button_ok;
+    private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+    private System.Windows.Forms.RadioButton radioButton1;
+    private System.Windows.Forms.RadioButton radioButton2;
+    private System.Windows.Forms.RadioButton radioButton3;
+    private System.Windows.Forms.Label label1;
   }
 }
