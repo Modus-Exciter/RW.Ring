6b1bad77cd66988301d5a2c503b2013fc7cd368e
Сергей Медведев <glorguint@mail.ru>
2024-05-03 13:19:46 +0300

    ANOVA parameters

--- source/Schicksal.Helm/Dialogs/AnovaOptionsDialog.Designer.cs
+++ source/Schicksal.Helm/Dialogs/AnovaOptionsDialog.Designer.cs
@@ -29,91 +29,92 @@ namespace Schicksal.Helm.Dialogs
     /// </summary>
     private void InitializeComponent()
     {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AnovaOptionsDialog));
-            this.m_bottom_panel = new System.Windows.Forms.FlowLayoutPanel();
-            this.m_button_cancel = new System.Windows.Forms.Button();
-            this.m_button_ok = new System.Windows.Forms.Button();
-            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-            this.radioButton1 = new System.Windows.Forms.RadioButton();
-            this.radioButton2 = new System.Windows.Forms.RadioButton();
-            this.radioButton3 = new System.Windows.Forms.RadioButton();
-            this.label1 = new System.Windows.Forms.Label();
-            this.m_bottom_panel.SuspendLayout();
-            this.tableLayoutPanel1.SuspendLayout();
-            this.SuspendLayout();
-            // 
-            // m_bottom_panel
-            // 
-            resources.ApplyResources(this.m_bottom_panel, "m_bottom_panel");
-            this.m_bottom_panel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.m_bottom_panel.Controls.Add(this.m_button_cancel);
-            this.m_bottom_panel.Controls.Add(this.m_button_ok);
-            this.m_bottom_panel.Name = "m_bottom_panel";
-            // 
-            // m_button_cancel
-            // 
-            resources.ApplyResources(this.m_button_cancel, "m_button_cancel");
-            this.m_button_cancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.m_button_cancel.Name = "m_button_cancel";
-            this.m_button_cancel.UseVisualStyleBackColor = true;
-            // 
-            // m_button_ok
-            // 
-            resources.ApplyResources(this.m_button_ok, "m_button_ok");
-            this.m_button_ok.DialogResult = System.Windows.Forms.DialogResult.OK;
-            this.m_button_ok.Name = "m_button_ok";
-            this.m_button_ok.UseVisualStyleBackColor = true;
-            // 
-            // tableLayoutPanel1
-            // 
-            resources.ApplyResources(this.tableLayoutPanel1, "tableLayoutPanel1");
-            this.tableLayoutPanel1.Controls.Add(this.radioButton1, 1, 0);
-            this.tableLayoutPanel1.Controls.Add(this.radioButton2, 2, 0);
-            this.tableLayoutPanel1.Controls.Add(this.radioButton3, 3, 0);
-            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
-            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            // 
-            // radioButton1
-            // 
-            resources.ApplyResources(this.radioButton1, "radioButton1");
-            this.radioButton1.Name = "radioButton1";
-            this.radioButton1.TabStop = true;
-            this.radioButton1.UseVisualStyleBackColor = true;
-            // 
-            // radioButton2
-            // 
-            resources.ApplyResources(this.radioButton2, "radioButton2");
-            this.radioButton2.Name = "radioButton2";
-            this.radioButton2.TabStop = true;
-            this.radioButton2.UseVisualStyleBackColor = true;
-            // 
-            // radioButton3
-            // 
-            resources.ApplyResources(this.radioButton3, "radioButton3");
-            this.radioButton3.Name = "radioButton3";
-            this.radioButton3.TabStop = true;
-            this.radioButton3.UseVisualStyleBackColor = true;
-            // 
-            // label1
-            // 
-            resources.ApplyResources(this.label1, "label1");
-            this.label1.Name = "label1";
-            // 
-            // AnovaOptionsDialog
-            // 
-            this.AcceptButton = this.m_button_ok;
-            resources.ApplyResources(this, "$this");
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.CancelButton = this.m_button_cancel;
-            this.Controls.Add(this.tableLayoutPanel1);
-            this.Controls.Add(this.m_bottom_panel);
-            this.Name = "AnovaOptionsDialog";
-            this.m_bottom_panel.ResumeLayout(false);
-            this.m_bottom_panel.PerformLayout();
-            this.tableLayoutPanel1.ResumeLayout(false);
-            this.tableLayoutPanel1.PerformLayout();
-            this.ResumeLayout(false);
-            this.PerformLayout();
+      System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AnovaOptionsDialog));
+      this.m_bottom_panel = new System.Windows.Forms.FlowLayoutPanel();
+      this.m_button_cancel = new System.Windows.Forms.Button();
+      this.m_button_ok = new System.Windows.Forms.Button();
+      this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+      this.m_btn_no_norm = new System.Windows.Forms.RadioButton();
+      this.m_btn_non_parametric = new System.Windows.Forms.RadioButton();
+      this.m_btn_box_cox = new System.Windows.Forms.RadioButton();
+      this.label1 = new System.Windows.Forms.Label();
+      this.m_bottom_panel.SuspendLayout();
+      this.tableLayoutPanel1.SuspendLayout();
+      this.SuspendLayout();
+      // 
+      // m_bottom_panel
+      // 
+      resources.ApplyResources(this.m_bottom_panel, "m_bottom_panel");
+      this.m_bottom_panel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+      this.m_bottom_panel.Controls.Add(this.m_button_cancel);
+      this.m_bottom_panel.Controls.Add(this.m_button_ok);
+      this.m_bottom_panel.Name = "m_bottom_panel";
+      // 
+      // m_button_cancel
+      // 
+      resources.ApplyResources(this.m_button_cancel, "m_button_cancel");
+      this.m_button_cancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+      this.m_button_cancel.Name = "m_button_cancel";
+      this.m_button_cancel.UseVisualStyleBackColor = true;
+      // 
+      // m_button_ok
+      // 
+      resources.ApplyResources(this.m_button_ok, "m_button_ok");
+      this.m_button_ok.DialogResult = System.Windows.Forms.DialogResult.OK;
+      this.m_button_ok.Name = "m_button_ok";
+      this.m_button_ok.UseVisualStyleBackColor = true;
+      // 
+      // tableLayoutPanel1
+      // 
+      resources.ApplyResources(this.tableLayoutPanel1, "tableLayoutPanel1");
+      this.tableLayoutPanel1.Controls.Add(this.m_btn_no_norm, 1, 0);
+      this.tableLayoutPanel1.Controls.Add(this.m_btn_non_parametric, 2, 0);
+      this.tableLayoutPanel1.Controls.Add(this.m_btn_box_cox, 3, 0);
+      this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+      this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+      // 
+      // m_btn_no_norm
+      // 
+      resources.ApplyResources(this.m_btn_no_norm, "m_btn_no_norm");
+      this.m_btn_no_norm.Checked = true;
+      this.m_btn_no_norm.Name = "m_btn_no_norm";
+      this.m_btn_no_norm.TabStop = true;
+      this.m_btn_no_norm.UseVisualStyleBackColor = true;
+      // 
+      // m_btn_non_parametric
+      // 
+      resources.ApplyResources(this.m_btn_non_parametric, "m_btn_non_parametric");
+      this.m_btn_non_parametric.Name = "m_btn_non_parametric";
+      this.m_btn_non_parametric.TabStop = true;
+      this.m_btn_non_parametric.UseVisualStyleBackColor = true;
+      // 
+      // m_btn_box_cox
+      // 
+      resources.ApplyResources(this.m_btn_box_cox, "m_btn_box_cox");
+      this.m_btn_box_cox.Name = "m_btn_box_cox";
+      this.m_btn_box_cox.TabStop = true;
+      this.m_btn_box_cox.UseVisualStyleBackColor = true;
+      // 
+      // label1
+      // 
+      resources.ApplyResources(this.label1, "label1");
+      this.label1.Name = "label1";
+      // 
+      // AnovaOptionsDialog
+      // 
+      this.AcceptButton = this.m_button_ok;
+      resources.ApplyResources(this, "$this");
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.CancelButton = this.m_button_cancel;
+      this.Controls.Add(this.tableLayoutPanel1);
+      this.Controls.Add(this.m_bottom_panel);
+      this.Name = "AnovaOptionsDialog";
+      this.m_bottom_panel.ResumeLayout(false);
+      this.m_bottom_panel.PerformLayout();
+      this.tableLayoutPanel1.ResumeLayout(false);
+      this.tableLayoutPanel1.PerformLayout();
+      this.ResumeLayout(false);
+      this.PerformLayout();
 
     }
 
@@ -123,9 +124,9 @@ namespace Schicksal.Helm.Dialogs
     private System.Windows.Forms.Button m_button_cancel;
     private System.Windows.Forms.Button m_button_ok;
     private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
-    private System.Windows.Forms.RadioButton radioButton1;
-    private System.Windows.Forms.RadioButton radioButton2;
-    private System.Windows.Forms.RadioButton radioButton3;
+    private System.Windows.Forms.RadioButton m_btn_no_norm;
+    private System.Windows.Forms.RadioButton m_btn_non_parametric;
+    private System.Windows.Forms.RadioButton m_btn_box_cox;
     private System.Windows.Forms.Label label1;
   }
 }
