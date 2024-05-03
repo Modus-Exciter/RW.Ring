a32d641b208005ef89eb8b9735f041241ab0eab6
Сергей Медведев <glorguint@mail.ru>
2024-05-03 13:19:46 +0300

    ANOVA parameters

--- source/Schicksal.Helm/Dialogs/AnovaOptionsDialog.cs
+++ source/Schicksal.Helm/Dialogs/AnovaOptionsDialog.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Xml;
 using System.Windows.Forms;
 
 namespace Schicksal.Helm.Dialogs
@@ -13,17 +14,42 @@ namespace Schicksal.Helm.Dialogs
   {
     public AnovaOptionsDialog()
     {
-      InitializeComponent();
+      this.InitializeComponent();
     }
 
     public string Save()
     {
-       return null;//throw new NotImplementedException();
+      StringBuilder sb = new StringBuilder();
+      sb.Append("<AnovaParameters Normalization=\"");
+      if (m_btn_no_norm.Checked)
+        sb.Append("None");
+      else if (m_btn_non_parametric.Checked)
+        sb.Append("NonParametric");
+      else if (m_btn_box_cox.Checked)
+        sb.Append("BoxCox");
+
+      sb.Append("\"/>");
+
+      return sb.ToString();
     }
 
     void IAnalysisOptions.Load(string xml)
     {
-      //throw new NotImplementedException();
+      if (string.IsNullOrWhiteSpace(xml))
+        return;
+
+      XmlDocument doc = new XmlDocument();
+      doc.LoadXml(xml);
+      switch (doc.DocumentElement.Attributes["Normalization"].Value)
+      {
+        case "NonParametric":
+          m_btn_non_parametric.Checked = true;
+          break;
+
+        case "BoxCox":
+          m_btn_box_cox.Checked = true;
+          break;
+      }
     }
 
     bool IAnalysisOptions.ShowDialog()
