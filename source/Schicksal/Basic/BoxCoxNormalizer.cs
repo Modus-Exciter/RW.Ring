39b8a8b7a03369fa33d83373d74f1f0707fb0c8d
Сергей Медведев <glorguint@mail.ru>
2024-07-26 19:41:24 +0300

    fix

--- source/Schicksal/Basic/BoxCoxNormalizer.cs
+++ source/Schicksal/Basic/BoxCoxNormalizer.cs
@@ -496,7 +496,7 @@ namespace Schicksal.Basic
 
       public override string ToString()
       {
-        return string.Format("Denormalizer(value => {0} value ^ {1}))", m_lambda, 1 / m_lambda);
+        return "Denormalizer(value => (lambda * value) ^ (1 / lambda))";
       }
     }
 
